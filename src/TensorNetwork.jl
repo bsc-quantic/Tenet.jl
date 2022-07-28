2b1762a6fd47c89066f625f8a932f7f3d347184e
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-07-28 19:05:40 +0200

    Update `TensorNetwork.jl` code

--- src/TensorNetwork.jl
+++ src/TensorNetwork.jl
@@ -1,10 +1,18 @@
-abstract type TensorNetwork end
+abstract type AbstractTensorNetwork end
 
-ntensors(_::TensorNetwork) = error("No implementation found")
-nindices(_::TensorNetwork) = error("No implementation found")
+ntensors(_::AbstractTensorNetwork) = error("No implementation found")
+nindices(_::AbstractTensorNetwork) = error("No implementation found")
+
+struct TensorNetwork <: AbstractTensorNetwork
+end
+
+ntensors(tn::TensorNetwork) = error("No implementation found")
+nindices(tn::TensorNetwork) = error("No implementation found")
 
 abstract type TensorNetworkState <: TensorNetwork end
 
-nsites(_::TensorNetworkState) <: TensorNetwork end
+nsites(tn::TensorNetworkState) = error("No mplementation found")
 
 abstract type TensorNetworkOperator <: TensorNetwork end
+
+nsites(_::TensorNetworkOperator) = error("No mplementation found")
