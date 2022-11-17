934b16edbce34493bdb2f331b5b28a0b4e6aeacd
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-11-17 23:49:10 +0100

    Add `inds`, `hyperinds` functions

--- src/TensorNetwork.jl
+++ src/TensorNetwork.jl
@@ -1,5 +1,6 @@
 import Base: length, show, summary
 using OptimizedEinsum
+using NamedDims
 
 struct TensorNetwork
     tensors::Dict{Int,NamedDimsArray}
@@ -8,3 +9,7 @@ end
 Base.length(x::TensorNetwork) = length(x.tensors)
 
 Base.show(io::IO, x::TensorNetwork) = println(io, "$(length(x))-tensors $(typeof(x))")
+
+inds(tn::TensorNetwork) = ∪(dimnames.(values(tn.tensors)))
+
+hyperinds(tn::TensorNetwork) = filter(ind -> count(∋(ind) ∘ dimnames, values(tn.tensors)) > 2, inds(tn))
