564f174462c5498e49a9b28b4eec9e2e6d94a06f
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-11-21 20:56:01 +0100

    Add `OptimizeEinsum.optimize` support for `TensorNetwork`

--- src/TensorNetwork.jl
+++ src/TensorNetwork.jl
@@ -1,17 +1,26 @@
 import Base: length, show, summary
-using OptimizedEinsum
+import OptimizedEinsum: optimize, Greedy
 using NamedDims
 
 struct TensorNetwork
-    tensors::Dict{Int,NamedDimsArray}
+    tensors::Vector{NamedDimsArray}
+    ind_size::Dict{Symbol,Int}
 end
 
 Base.length(x::TensorNetwork) = length(x.tensors)
 
 Base.summary(io::IO, x::TensorNetwork) = println(io, "$(length(x))-tensors TensorNetwork")
 
-inds(tn::TensorNetwork) = ∪(dimnames.(values(tn.tensors)))
+inds(tn::TensorNetwork) = keys(tn.ind_size)
 
 openinds(tn::TensorNetwork) = filter(ind -> count(∋(ind) ∘ dimnames, values(tn.tensors)) == 1, inds(tn))
 
 hyperinds(tn::TensorNetwork) = filter(ind -> count(∋(ind) ∘ dimnames, values(tn.tensors)) > 2, inds(tn))
+
+function optimize(opt, tn::TensorNetwork; output=openinds(tn))
+    inputs = dimnames.(tn.tensors)
+    size = tn.ind_size
+    optimize(opt, tn.tensors, output, size)
+end
+
+optimize(tn::TensorNetwork; kwargs...) = optimize(Greedy, tn; kwargs...)
