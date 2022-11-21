49e89ab8f0a61b5b8c68309d67cb3b12f660d7a1
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-11-21 12:59:21 +0100

    Add `openinds` function

--- src/TensorNetwork.jl
+++ src/TensorNetwork.jl
@@ -12,4 +12,6 @@ Base.summary(io::IO, x::TensorNetwork) = println(io, "$(length(x))-tensors Tenso
 
 inds(tn::TensorNetwork) = ∪(dimnames.(values(tn.tensors)))
 
+openinds(tn::TensorNetwork) = filter(ind -> count(∋(ind) ∘ dimnames, values(tn.tensors)) == 1, inds(tn))
+
 hyperinds(tn::TensorNetwork) = filter(ind -> count(∋(ind) ∘ dimnames, values(tn.tensors)) > 2, inds(tn))
