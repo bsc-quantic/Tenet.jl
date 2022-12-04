44ecef3751372568da68cbc5c1ca846ff3e09902
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-12-04 20:39:52 +0100

    Integrate Quac circuit conversion to `TensorNetwork`

---
+++ src/Quantum.jl
@@ -0,0 +1,34 @@
+using Quac
+using OptimizedEinsum: get_symbol
+using NamedDims
+
+function TensorNetwork(circuit::Circuit)
+    tn = TensorNetwork()
+    n = lanes(circuit)
+
+    wire = [get_symbol(i) for i in 1:n]
+    i = n + 1
+
+    for gate in circuit
+        G = arraytype(gate)
+        array = G(gate)
+
+        if gate isa Swap
+            (a, b) = lanes(gate)
+            wire[a], wire[b] = wire[b], wire[a]
+            continue
+        end
+
+        inds = map(lanes(gate)) do l
+            from, to = wire[l], get_symbol(i)
+            i += 1
+            wire[l] = to
+            (from, to)
+        end |> x -> zip(x...) |> Iterators.flatten |> collect
+
+        tensor = NamedDimsArray{tuple(inds...)}(array)
+        push!(tn, tensor)
+    end
+
+    return tn
+end
--- src/Tenet.jl
+++ src/Tenet.jl
@@ -3,6 +3,8 @@ module Tenet
 include("TensorNetwork.jl")
 export TensorNetwork, tensors, arrays, inds, openinds, hyperinds
 
+include("Quantum.jl")
+
 include("Visualization.jl")
 export draw
 
