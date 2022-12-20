ded6e8bf92903f676f11fdb7e91aa2280e784fed
Jofre <jofrevalles99@gmail.com>
2022-12-20 15:17:08 +0100

    Reorganize Project.toml

--- src/Tenet.jl
+++ src/Tenet.jl
@@ -1,5 +1,7 @@
 module Tenet
 
+using Requires: @require
+
 include("TensorNetwork.jl")
 export TensorNetwork, tensors, arrays, inds, openinds
 
@@ -8,7 +10,6 @@ export GenericTensorNetwork
 
 include("Quantum.jl")
 
-using Requires: @require
 function __init__()
     @require Makie = "ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" include("Visualization.jl")
 end
