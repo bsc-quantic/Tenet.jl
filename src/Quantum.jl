98199b6e03d1c88dd07391b6fdc1dd20904ad976
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-01-23 22:29:42 +0100

    Add `sites`,`insites`,`outsites` functions

--- src/Quantum.jl
+++ src/Quantum.jl
@@ -1,5 +1,6 @@
 using Quac
 using OptimizedEinsum: get_symbol
+using LinearAlgebra: Adjoint
 
 """
     Quantum
@@ -24,11 +25,24 @@ Tensor Networks that have both inputs and outputs. Generally, they represent evo
 """
 abstract type Operator <: Quantum end
 
+sites(tn::TensorNetwork) = insites(tn) ∪ outsites(tn)
+
+# TODO maybe don't filter by openinds?
+insites(tn::TensorNetwork) = Set(site.(insiteinds(tn)))
+insites(::TensorNetwork{<:State}) = throw(MethodError(insites, TensorNetwork{<:State}))
+insites(tn::Adjoint{TensorNetwork}) = outsites(parent(tn))
+insiteinds(tn) = filter(i -> i.meta[:plug] == :input, openinds(tn))
+
+# TODO maybe don't filter by openinds?
+outsites(tn::TensorNetwork) = Set(site.(outsiteinds(tn)))
+outsites(tn::Adjoint{TensorNetwork}) = insites(parent(tn))
+outsiteinds(tn) = filter(i -> i.meta[:plug] == :output, openinds(tn))
+
 function TensorNetwork(circuit::Circuit)
-    tn = TensorNetwork()
+    tn = TensorNetwork{Quantum}()
     n = lanes(circuit)
 
-    wire = [get_symbol(i) for i in 1:n]
+    wire = [[get_symbol(i)] for i in 1:n]
     i = n + 1
 
     for gate in circuit
@@ -42,14 +56,28 @@ function TensorNetwork(circuit::Circuit)
         end
 
         inds = map(lanes(gate)) do l
-            from, to = wire[l], get_symbol(i)
+            from, to = last(wire[l]), get_symbol(i)
             i += 1
-            wire[l] = to
+            push!(wire[l], to)
             (from, to)
         end |> x -> zip(x...) |> Iterators.flatten |> collect
 
-        tensor = Tensor(array, tuple(inds...))
+        tensor = Tensor(array, tuple(inds...); gate = gate)
         push!(tn, tensor)
+    end
+
+    for (lane, wireindices) in enumerate(wire)
+        for index in wireindices
+            tn.inds[index].meta[:site] = lane
+        end
+    end
+
+    for input in first.(wire)
+        tn.inds[input].meta[:plug] = :input
+    end
+
+    for output in last.(wire)
+        tn.inds[output].meta[:plug] = :output
     end
 
     return tn
--- src/Tenet.jl
+++ src/Tenet.jl
@@ -22,7 +22,7 @@ export transform, transform!
 
 include("Quantum.jl")
 export Quantum
-export physicalinds, virtualinds
+export physicalinds, virtualinds, sites, insites, insiteinds, outsites, outsiteinds
 
 function __init__()
     @require Makie = "ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" include("Visualization.jl")
