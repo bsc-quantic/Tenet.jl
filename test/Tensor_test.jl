0d2c97d9712600f371c71a4ae2311018de359274
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-01-25 18:47:12 +0100

    Test `reindex(::Tensor)`

--- test/Tensor_test.jl
+++ test/Tensor_test.jl
@@ -7,6 +7,17 @@
         @test labels(Tensor(zeros(2, 2, 2), [:i, :j, :k])) == (:i, :j, :k)
     end
 
+    @testset "reindex" begin
+        using Tenet: reindex
+
+        tensor = Tensor(zeros(2, 2, 2), (:i, :j, :k))
+        @test labels(reindex(tensor, :i => :u, :j => :v, :k => :w)) == (:u, :v, :w)
+        @test parent(reindex(tensor, :i => :u, :j => :v, :k => :w)) === parent(tensor)
+
+        @test labels(reindex(tensor, :a => :u, :b => :v, :c => :w)) == (:i, :j, :k)
+        @test parent(reindex(tensor, :a => :u, :b => :v, :c => :w)) === parent(tensor)
+    end
+
     @testset "Broadcasting" begin
         data = rand(2, 2, 2)
         @test begin
