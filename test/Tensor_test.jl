f167476f78581d9f01949e5bea39783d9b6bd533
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-01-25 18:54:57 +0100

    Test `tags`,`tag!`,`untag!`,`hastag` for `Tensor`

--- test/Tensor_test.jl
+++ test/Tensor_test.jl
@@ -46,4 +46,20 @@
             parent(tensor) == sin.(data)
         end
     end
+
+    @testset "tags" begin
+        using Tenet: tags, tag!, untag!, hastag
+
+        tensor = Tensor(zeros(2, 2, 2), (:i, :j, :k), tags = Set{String}(["TAG_A", "TAG_B"]))
+
+        @test issetequal(tags(tensor), ["TAG_A", "TAG_B"])
+
+        tag!(tensor, "TAG_C")
+        @test hastag(tensor, "TAG_C")
+
+        untag!(tensor, "TAG_C")
+        @test !hastag(tensor, "TAG_C")
+
+        @test untag!(tensor, "TAG_UNEXISTANT") == tags(tensor)
+    end
 end
