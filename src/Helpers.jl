0c935e61b7f8cbb40ce39201732a05ca846b881f
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-01-30 15:29:28 +0100

    Add `RingPeek` iterator

--- src/Helpers.jl
+++ src/Helpers.jl
@@ -1,1 +1,36 @@
+using Base.Iterators: Cycle, cycle
+
 const Sequence{T} = Union{AbstractArray{T,1},NTuple{N,T} where {N}}
+
+mutable struct RingPeek{Itr<:Cycle}
+    const it::Itr
+    base::Any
+end
+
+ringpeek(itr) = RingPeek(cycle(itr), nothing)
+ringpeek(itr::Itr) where {Itr<:Cycle} = RingPeek(itr, nothing)
+
+period(itr::Cycle) = length(itr.xs)
+Base.IteratorSize(::Type{RingPeek{Itr}}) where {Itr} = Base.HasLength()
+Base.length(itr::RingPeek{Itr}) where {Itr} = period(itr.it)
+
+Base.IteratorEltype(::Type{RingPeek{Itr}}) where {Itr} = Base.IteratorEltype(Itr)
+Base.eltype(::Type{RingPeek{Itr}}) where {Itr} = Tuple{eltype(Itr),eltype(Itr)}
+
+Base.isdone(it::RingPeek, state) = it.base == state
+
+function Base.iterate(it::RingPeek{Itr}) where {Itr}
+    x, state = iterate(it.it)
+    peeked, nextstate = iterate(it.it, state)
+    it.base = state
+    ((x, peeked), state)
+end
+
+function Base.iterate(it::RingPeek{Itr}, state) where {Itr}
+    x, newstate = iterate(it.it, state)
+    peeked, _ = iterate(it.it, newstate)
+
+    newstate == it.base && return nothing
+
+    ((x, peeked), newstate)
+end
--- test/Helpers_test.jl
+++ test/Helpers_test.jl
@@ -1,7 +1,17 @@
 @testset "Helpers" begin
-    @test Vector <: Tenet.Sequence
-    @test (Vector{T} where {T}) <: Tenet.Sequence{T} where {T}
-    @test NTuple <: Tenet.Sequence
-    @test (NTuple{N} where {N}) <: Tenet.Sequence
-    @test (NTuple{N,T} where {N,T}) <: Tenet.Sequence where {T}
+    @testset "Sequence" begin
+        @test Vector <: Tenet.Sequence
+        @test (Vector{T} where {T}) <: Tenet.Sequence{T} where {T}
+        @test NTuple <: Tenet.Sequence
+        @test (NTuple{N} where {N}) <: Tenet.Sequence
+        @test (NTuple{N,T} where {N,T}) <: Tenet.Sequence where {T}
+    end
+
+    @testset "RingPeek" begin
+        using Tenet: ringpeek
+
+        it = ringpeek([0 1 2])
+        @test length(it) == 3
+        @test collect(it) == [(0, 1), (1, 2), (2, 0)]
+    end
 end
