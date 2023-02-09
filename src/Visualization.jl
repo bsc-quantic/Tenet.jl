83b75c374e1b8ab4674de7473189a53509924baf
Jofre <jofrevalles99@gmail.com>
2023-02-09 17:36:50 +0100

    Refactor visualization functions

--- src/Visualization.jl
+++ src/Visualization.jl
@@ -1,23 +1,46 @@
 using Graphs: SimpleGraph, Edge, nv
-using GraphMakie: graphplot!, GraphPlot
+using GraphMakie: graphplot!, GraphPlot, to_colormap, get_node_plot
 using Combinatorics: combinations
+using GraphMakie.NetworkLayout: IterativeLayout
 import Makie
 
-Makie.plottype(::TensorNetwork) = GraphPlot
+const MAX_NODE_SIZE = 20.
 
-function Makie.plot!(P::GraphPlot{Tuple{TensorNetwork{A}}}; kwargs...) where {A<:Ansatz}
-    tn = P[1][]
+function Makie.plot(tn::TensorNetwork{A}; kwargs...) where {A<:Ansatz}
+    f = Makie.Figure()
+
+    p, ax = Makie.plot!(f[1,1], tn; kwargs...)
+    display(f)
+
+    return f, ax, p
+end
+
+function Makie.plot!(f::Makie.GridPosition, tn::TensorNetwork{A}; kwargs...) where {A<:Ansatz}
+    scene = Makie.Scene()
+    default_attrs = Makie.default_theme(scene, GraphPlot)
+
+    kwargs = Dict{Symbol,Any}(kwargs)
 
     pos = IdDict(tensor => i for (i, tensor) in enumerate(tensors(tn)))
     graph = SimpleGraph([Edge(pos[a], pos[b]) for ind in inds(tn) for (a, b) in combinations(links(ind), 2)])
 
-    scene = Makie.parent_scene(P)
-    default_attrs = Makie.default_theme(scene, GraphPlot)
+    log_size = [log2(size(tensors(tn, i)) |> prod) for i in 1:nv(graph)]
 
-    kwargs = Dict{Symbol,Any}(kwargs)
-    if P.attributes.node_size[] == default_attrs.node_size[]
-        kwargs[:node_size] = [max(10, log2(size(tensors(tn, i)) |> prod)) for i in 1:nv(graph)]
+    min_size, max_size = extrema(log_size)
+
+    kwargs[:node_size] = (log_size/max_size) * MAX_NODE_SIZE
+
+    if haskey(kwargs, :layout) && kwargs[:layout] isa IterativeLayout{3}
+        ax = Makie.LScene(f[1,1])
+    else
+        ax = Makie.Axis(f[1,1])
+        # hide decorations if it is not a 3D plot
+        Makie.hidedecorations!(ax)
+        Makie.hidespines!(ax)
+        ax.aspect = DataAspect()
     end
 
-    graphplot!(P, graph; kwargs...)
+    p = graphplot!(f[1,1], graph; kwargs...)
+
+    return p, ax
 end
