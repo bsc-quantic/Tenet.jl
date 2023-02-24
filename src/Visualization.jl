9367923ecbf275b6e5381eacbbe0e50e5dae6358
Jofre Vallès Muns <61060572+jofrevalles@users.noreply.github.com>
2023-02-24 11:29:51 +0100

    Fix plots for `TensorNetworks` with hyperedges (#31)
    * Fix plots for TensorNetworks with hyperedges
    
    * Small fixes from last commit
    
    * Change markers and colors
    
    * Apply @mofeing suggestions from code review
    
    Co-authored-by: Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>

--- src/Visualization.jl
+++ src/Visualization.jl
@@ -17,13 +17,36 @@ function Makie.plot!(f::Makie.GridPosition, tn::TensorNetwork{A}; labels = false
     scene = Makie.Scene()
     default_attrs = Makie.default_theme(scene, GraphPlot)
 
+    tn = transform(tn, HyperindConverter)
+
     kwargs = Dict{Symbol,Any}(kwargs)
 
     pos = IdDict(tensor => i for (i, tensor) in enumerate(tensors(tn)))
     graph = SimpleGraph([Edge(pos[a], pos[b]) for ind in inds(tn) for (a, b) in combinations(links(ind), 2)])
 
-    kwargs[:node_size] = [max(10, log2(size(tensors(tn,i)) |> prod)) for i in 1:nv(graph)]
-    elabels = [join(Tenet.labels(tensors(tn)[edge.src]) ∩ Tenet.labels(tensors(tn)[edge.dst]), ',') for edge in edges(graph)]
+    # TODO recognise them by using `DeltaArray` or `Diagonal` representations
+    copytensors = findall(t -> haskey(t.meta, :dual), tensors(tn))
+
+    kwargs[:node_size] = [max(15, log2(size(tensors(tn,i)) |> prod)) for i in 1:nv(graph)]
+    kwargs[:node_marker] = [i ∈ copytensors ? :diamond : :circle for i in 1:length(tensors(tn))]
+    kwargs[:node_color] = [i ∈ copytensors ? :black : :white for i in 1:length(tensors(tn))]
+
+    if labels
+        elabels = Vector{String}([])
+        elabels_color = Vector{Symbol}([])
+
+        for edge in edges(graph)
+            copies = filter((x -> x ∈ copytensors), [edge.src, edge.dst])
+
+            if isempty(copies) # there are no copy tensors in the nodes of this edge
+                push!(elabels, join(Tenet.labels(tensors(tn)[edge.src]) ∩ Tenet.labels(tensors(tn)[edge.dst]), ','))
+                push!(elabels_color, :black)
+            else
+                push!(elabels, string(tensors(tn)[copies[]].meta[:dual]))
+                push!(elabels_color, :grey)
+            end
+        end
+    end
 
     if haskey(kwargs, :layout) && kwargs[:layout] isa IterativeLayout{3}
         ax = Makie.LScene(f[1,1])
@@ -37,9 +60,9 @@ function Makie.plot!(f::Makie.GridPosition, tn::TensorNetwork{A}; labels = false
 
     p = graphplot!(f[1,1], graph;
         elabels = labels ? elabels : nothing,
-        elabels_color = [:black for i in 1:ne(graph)],
         # TODO configurable `elabels_textsize`
         elabels_textsize = [17 for i in 1:ne(graph)],
+        node_attr=(colormap = :viridis, strokewidth = 2., strokecolor = :black),
         kwargs...)
 
     return p, ax
