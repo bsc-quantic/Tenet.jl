4360929683c5218b4d1a4c6cf85f3228fa8d5412
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-02-25 01:59:52 +0100

    Format document

--- docs/make.jl
+++ docs/make.jl
@@ -7,13 +7,13 @@ push!(LOAD_PATH, "$(@__DIR__)/..")
 using Documenter
 using Tenet
 
-DocMeta.setdocmeta!(Tenet, :DocTestSetup, :(using Tenet); recursive=true)
+DocMeta.setdocmeta!(Tenet, :DocTestSetup, :(using Tenet); recursive = true)
 
 makedocs(
-    modules=[Tenet],
-    sitename="Tenet.jl",
-    authors="Sergio Sánchez Ramírez and contributors",
-    pages=Any[
+    modules = [Tenet],
+    sitename = "Tenet.jl",
+    authors = "Sergio Sánchez Ramírez and contributors",
+    pages = Any[
         "Home"=>"index.md",
         "Tensor"=>"tensor.md",
         "Alternatives"=>"alternatives.md",
