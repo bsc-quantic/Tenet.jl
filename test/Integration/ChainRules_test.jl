0d7d152d785764347d474f500557347f0e3a58ad
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-03-01 11:07:39 -0500

    Add tests for `ChainRulesCore`

--- test/Integration/ChainRules_test.jl
+++ test/Integration/ChainRules_test.jl
@@ -1,7 +1,16 @@
 @testset "ChainRules" begin
     using Tenet: Tensor, contract
+    using Random
+
     using ChainRulesTestUtils
     ChainRulesTestUtils.ChainRulesCore.debug_mode() = true
+
+    using ChainRulesCore
+    function ChainRulesTestUtils.rand_tangent(rng::AbstractRNG, x::TensorNetwork)
+        return ProjectTo(x)(
+            TensorNetwork([ProjectTo(tensor)(rand_tangent.(Ref(rng), tensor)) for tensor in tensors(x)]),
+        )
+    end
 
     @testset "Tensor" begin
         test_rrule(Tensor, rand(2, 2), (:i, :j))
@@ -41,5 +50,23 @@
             test_frule(contract, a, b)
             test_rrule(contract, a, b)
         end
+
+        # TODO complete tests
+        @testset "TensorNetwork" begin
+            tn = rand(TensorNetwork, 10, 3)
+
+            # test_frule(contract, tn)
+            # test_rrule(contract, tn)
+        end
+    end
+
+    @testset "replace" begin
+        using UUIDs: uuid4
+
+        tn = rand(TensorNetwork, 10, 3)
+        mapping = [label => Symbol(uuid4()) for label in labels(tn)]
+
+        # TODO fails in check_result.jl@161 -> `c_actual = collect(Broadcast.materialize(actual))`
+        # test_rrule(replace, tn, mapping...)
     end
 end
