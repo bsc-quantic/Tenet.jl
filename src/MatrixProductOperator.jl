90c02af031514483e298a57413886a94dbd97435
epuljak <ema.puljak@cern.ch>
2023-03-09 02:03:29 +0100

    Create MPO

---
+++ src/MatrixProductOperator.jl
@@ -0,0 +1,175 @@
+using OptimizedEinsum: ContractionPath
+using UUIDs: uuid4
+using IterTools: partition
+using Random
+
+abstract type MatrixProductOperator{B} <: State{B} end
+
+function MatrixProductOperator(arrays; bounds::Type{<:Bounds} = Open, kwargs...)
+    MatrixProductOperator{bounds}(arrays; kwargs...)
+end
+
+function MatrixProductOperator{Open}(arrays; χ = nothing, order = (:l, :r, :p), meta...)
+    !issetequal(order, (:l, :r, :p)) && throw(ArgumentError("`order` must be a permutation of the :l, :r and :p"))
+    order = Dict(dim => i for (i, dim) in enumerate(order))
+
+    # check format
+    # NOTE matching index dimensions are checked on `push!(::TensorNetwork,...)`
+    # TODO check χ?
+    if !all(==(4) ∘ ndims, arrays[2:end-1]) || ndims(first(arrays)) != 3 || ndims(last(arrays)) != 3
+        throw(DimensionMismatch("virtual bond-dims mismatch"))
+    end
+
+    tn = TensorNetwork{MatrixProductOperator{Open}}(; χ = χ, order = order, meta...)
+
+    n = length(arrays)
+    vinds = Dict(x => Symbol(uuid4()) for x in partition(1:n, 2, 1))
+    oinds = Dict(i => Symbol(uuid4()) for i in 1:n)
+    iinds = Dict(i => Symbol(uuid4()) for i in 1:n)
+    permutator = [order[i] for i in (:l, :r, :p)]
+
+    # add boundary tensors
+    # first
+    labels = if order[:r] < order[:p]
+        (vinds[(1, 2)], iinds[1], oinds[1])
+    else
+        (iinds[1], oinds[1], vinds[(1, 2)])
+    end
+    push!(tn, Tensor(first(arrays), labels))
+    # last
+    labels = if order[:l] < order[:p]
+        (vinds[(n - 1, n)], iinds[n], oinds[n])
+    else
+        (iinds[n], oinds[n], vinds[(n - 1, n)])
+    end
+    push!(tn, Tensor(last(arrays), labels))
+
+    # add other tensors
+    for (i, data) in zip(2:n-1, arrays[2:end-1])
+        lind = vinds[(mod1(i - 1, n), i)]
+        rind = vinds[(i, mod1(i + 1, n))]
+
+        labels = [lind, rind, iinds[i], oinds[i]]
+        permute!(labels, permutator)
+
+        tensor = Tensor(data, labels)
+        push!(tn, tensor)
+    end
+
+    # mark input indices
+    for (site, label) in iinds
+        index = inds(tn, label)
+        index.meta[:site] = site
+        index.meta[:plug] = :input
+    end
+    # mark output indices
+    for (site, label) in oinds
+        index = inds(tn, label)
+        index.meta[:site] = site
+        index.meta[:plug] = :output
+    end
+
+    return tn
+end
+
+function MatrixProductOperator{Closed}(arrays; χ = nothing, order = (:l, :r, :p), meta...)
+    !issetequal(order, (:l, :r, :p)) && throw(ArgumentError("`order` must be a permutation of the :l, :r and :p"))
+    order = Dict(side => i for (i, side) in enumerate(order))
+
+    # check format
+    # TODO check χ?
+    if !all(==(4) ∘ ndims, arrays)
+        throw(DimensionMismatch("virtual bond-dims mismatch"))
+    end
+
+    tn = TensorNetwork{MatrixProductState{Closed}}(; χ = χ, order = order, meta...)
+
+    n = length(arrays)
+    vinds = Dict(x => Symbol(uuid4()) for x in ringpeek(1:n))
+    oinds = Dict(i => Symbol(uuid4()) for i in 1:n)
+    iinds = Dict(i => Symbol(uuid4()) for i in 1:n)
+    permutator = [order[i] for i in (:l, :r, :p)]
+
+    # add tensors
+    for (i, data) in enumerate(arrays)
+        lind = vinds[(mod1(i - 1, n), i)]
+        rind = vinds[(i, mod1(i + 1, n))]
+
+        labels = [lind, rind, iinds[i], oinds[i]]
+        permute!(labels, permutator)
+
+        tensor = Tensor(data, labels)
+        push!(tn, tensor)
+    end
+
+    # mark input indices
+    for (site, label) in iinds
+        index = inds(tn, label)
+        index.meta[:site] = site
+        index.meta[:plug] = :input
+    end
+    # mark output indices
+    for (site, label) in oinds
+        index = inds(tn, label)
+        index.meta[:site] = site
+        index.meta[:plug] = :output
+    end
+
+    return tn
+end
+ 
+
+struct MPOSampler{B<:Bounds,T} <: Random.Sampler{TensorNetwork{MatrixProductOperator{B}}}
+    n::Int
+    ip::Int
+    op::Int
+    χ::Int
+end
+
+Base.eltype(::MPOSampler{B}) where {B<:Bounds} = TensorNetwork{MatrixProductOperator{B}}
+
+function Base.rand(
+    ::Type{MatrixProductState{B}},
+    n::Integer,
+    ip::Integer,
+    op::Integer,
+    χ::Integer;
+    eltype::Type = Float32,
+) where {B<:Bounds}
+    rand(MPOSampler{B,eltype}(n, ip, op, χ))
+end
+
+Base.rand(::Type{MatrixProductOperator}, args...; kwargs...) = rand(MatrixProductOperator{Open}, args...; kwargs...)
+
+# TODO let choose the orthogonality center
+# function Base.rand(rng::Random.AbstractRNG, sampler::MPOSampler{Open,T}) where {T}
+#     n, χ, ip, op = getfield.((sampler,), (:n, :χ, :ip, :op))
+
+#     arrays::Vector{AbstractArray{T,N} where {N}} = map(1:n) do i
+#         χl, χr = let after_mid = i < n ÷ 2, i = (n - 1 - abs(2i - n)) ÷ 2
+#             χl = min(χ, ip^(i - 1) * op^(i + 1))
+#             χr = min(χ, ip^i * op^())
+
+#             # swap bond dims after mid
+#             after_mid ? (χr, χl) : (χl, χr)
+#         end
+
+#         # fix for first site
+#         i == 1 && ((χl, χr) = (χr, 1))
+
+#         # orthogonalize by Gram-Schmidt algorithm
+#         A = gramschmidt!(rand(rng, T, χl, χr * p))
+
+#         reshape(A, χl, χr, p)
+#     end
+
+
+    
+#     MatrixProductOperator{Open}(arrays; χ = χ)
+# end
+
+# TODO stable renormalization
+function Base.rand(rng::Random.AbstractRNG, sampler::MPOSampler{Closed,T}) where {T}
+    n, χ, ip, op = getfield.((sampler,), (:n, :χ, :ip, :op))
+    MatrixProductOperator{Closed}([rand(rng, T, n, χ, ip, op) for _ in 1:n]; χ = χ)
+end
---
+++ test/MatrixProductOperator_test.jl
@@ -0,0 +1,76 @@
+@testset "MatrixProductState" begin
+    using Tenet: TensorNetwork, State, Closed, Open, bounds, MatrixProductOperator
+
+    @testset "Types" begin
+        @test MatrixProductOperator <: State
+        @test all(T -> MatrixProductOperator{T} <: State, [Open, Closed])
+        @test all(T -> MatrixProductOperator{T} <: State{T}, [Open, Closed])
+        @test all(B -> bounds(MatrixProductOperator{B}) == B, [Open, Closed])
+    end
+
+    @testset "Constructor" begin
+        # empty constructor
+
+        # @test_skip begin
+        #     arrays = [rand(3)]
+        #     MatrixProductOperator(arrays)
+        # end
+
+        @test_skip begin
+            arrays = [rand(1, 3, 1), rand(1, 3, 1)]
+            MatrixProductOperator(arrays) isa TensorNetwork{MatrixProductOperator{Open}}
+        end
+
+        @testset "`Open` boundary" begin
+            # product operator
+            @test begin
+                arrays = [rand(1, 3, 1), rand(3, 3, 1, 1), rand(3, 1, 1)]
+                MatrixProductOperator{Open}(arrays) isa TensorNetwork{MatrixProductOperator{Open}}
+            end
+
+            # # entangled state
+            # @test begin
+            #     arrays = [rand(2, 2), rand(2, 4, 2), rand(4, 1, 2), rand(1, 2)]
+            #     MatrixProductState{Open}(arrays) isa TensorNetwork{MatrixProductState{Open}}
+            # end
+
+            # alternative constructor
+            @test begin
+                arrays = [rand(1, 3, 1), rand(3, 3, 1, 1), rand(3, 1, 1)]
+                MatrixProductOperator(arrays; bounds = Open) isa TensorNetwork{MatrixProductOperator{Open}}
+            end
+
+            # # fail on Open with Closed format
+            # @test_throws DimensionMismatch begin
+            #     arrays = [rand(1, 1, 2), rand(1, 1, 2), rand(1, 1, 2)]
+            #     MatrixProductState{Open}(arrays) isa TensorNetwork{MatrixProductState{Open}}
+            # end
+        end
+
+        # @testset "`Closed` boundary" begin
+        #     # product state
+        #     @test begin
+        #         arrays = [rand(1, 1, 2), rand(1, 1, 2), rand(1, 1, 2)]
+        #         MatrixProductState{Closed}(arrays) isa TensorNetwork{MatrixProductState{Closed}}
+        #     end
+
+        #     # entangled state
+        #     @test begin
+        #         arrays = [rand(3, 4, 2), rand(4, 8, 2), rand(8, 3, 2)]
+        #         MatrixProductState{Closed}(arrays) isa TensorNetwork{MatrixProductState{Closed}}
+        #     end
+
+        #     # alternative constructor
+        #     @test begin
+        #         arrays = [rand(1, 1, 2), rand(1, 1, 2), rand(1, 1, 2)]
+        #         MatrixProductState(arrays; bounds = Closed) isa TensorNetwork{MatrixProductState{Closed}}
+        #     end
+
+        #     # fail on Closed with Open format
+        #     @test_throws DimensionMismatch begin
+        #         arrays = [rand(1, 2), rand(1, 1, 2), rand(1, 2)]
+        #         MatrixProductState{Closed}(arrays) isa TensorNetwork{MatrixProductState{Closed}}
+        #     end
+        # end
+    end
+end
