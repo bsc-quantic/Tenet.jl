ce1c1ca8e2bfdcd468da5794bbc08aa1a22c8134
Jofre <jofrevalles99@gmail.com>
2023-03-20 12:10:17 +0100

    Test return type for plot functions

--- test/Visualization_test.jl
+++ test/Visualization_test.jl
@@ -2,6 +2,7 @@
     using CairoMakie
     using Tenet
     using NetworkLayout: Spring
+    using Makie: FigureAxisPlot, AxisPlot
 
     @testset "plot`" begin
         tn = TensorNetwork([
@@ -10,23 +11,23 @@
             Tensor(rand(2),(:x,))
             ])
 
-        @test plot(tn) isa Any
-        @test plot(tn; labels=true) isa Any
-        @test plot(tn; layout=Spring(dim=3)) isa Any
+        @test plot(tn) isa FigureAxisPlot
+        @test plot(tn; labels=true) isa FigureAxisPlot
+        @test plot(tn; layout=Spring(dim=3)) isa FigureAxisPlot
 
         @test begin
             f = Figure()
-            plot!(f[1,1], tn) isa Any
+            plot!(f[1,1], tn) isa AxisPlot
         end
 
         @test begin
             f = Figure()
-            plot!(f[1,1], tn; labels=true) isa Any
+            plot!(f[1,1], tn; labels=true) isa AxisPlot
         end
 
         @test begin
             f = Figure()
-            plot!(f[1,1], tn; layout=Spring(dim=3)) isa Any
+            plot!(f[1,1], tn; layout=Spring(dim=3)) isa AxisPlot
         end
     end
 end
