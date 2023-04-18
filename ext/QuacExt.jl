121eb6bbd5054016e52acbc6b77746dafd230e83
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+github@bsc.es>
2023-04-18 22:31:37 +0200

    Move `Quac` code to Package Extension

---
+++ ext/QuacExt.jl
@@ -0,0 +1,51 @@
+module QuacExt
+
+using Quac: Circuit, lanes, arraytype, Swap
+using Tenet: TensorNetwork, Quantum, Tensor
+
+function TensorNetwork(circuit::Circuit)
+    tn = TensorNetwork{Quantum}()
+    n = lanes(circuit)
+
+    wire = [[get_symbol(i)] for i in 1:n]
+    i = n + 1
+
+    for gate in circuit
+        G = arraytype(gate)
+        array = G(gate)
+
+        if gate isa Swap
+            (a, b) = lanes(gate)
+            wire[a], wire[b] = wire[b], wire[a]
+            continue
+        end
+
+        inds = map(lanes(gate)) do l
+            from, to = last(wire[l]), get_symbol(i)
+            i += 1
+            push!(wire[l], to)
+            (from, to)
+        end |> x -> zip(x...) |> Iterators.flatten |> collect
+
+        tensor = Tensor(array, tuple(inds...); gate = gate)
+        push!(tn, tensor)
+    end
+
+    for (lane, wireindices) in enumerate(wire)
+        for index in wireindices
+            tn.inds[index].meta[:site] = lane
+        end
+    end
+
+    for input in first.(wire)
+        tn.inds[input].meta[:plug] = :input
+    end
+
+    for output in last.(wire)
+        tn.inds[output].meta[:plug] = :output
+    end
+
+    return tn
+end
+
+end
--- src/Quantum/Quantum.jl
+++ src/Quantum/Quantum.jl
@@ -1,4 +1,3 @@
-using Quac: Circuit, lanes, arraytype, Swap
 using OptimizedEinsum: get_symbol
 using LinearAlgebra
 using UUIDs: uuid4
@@ -54,51 +53,6 @@ outsiteind(tn, s) = only(filter(i -> site(i) == s, outsiteinds(tn)))
 
 physicalinds(tn::TensorNetwork) = Iterators.filter(isphysical, inds(tn)) |> collect
 virtualinds(tn::TensorNetwork) = Iterators.filter(isvirtual, inds(tn)) |> collect
-
-function TensorNetwork(circuit::Circuit)
-    tn = TensorNetwork{Quantum}()
-    n = lanes(circuit)
-
-    wire = [[get_symbol(i)] for i in 1:n]
-    i = n + 1
-
-    for gate in circuit
-        G = arraytype(gate)
-        array = G(gate)
-
-        if gate isa Swap
-            (a, b) = lanes(gate)
-            wire[a], wire[b] = wire[b], wire[a]
-            continue
-        end
-
-        inds = map(lanes(gate)) do l
-            from, to = last(wire[l]), get_symbol(i)
-            i += 1
-            push!(wire[l], to)
-            (from, to)
-        end |> x -> zip(x...) |> Iterators.flatten |> collect
-
-        tensor = Tensor(array, tuple(inds...); gate = gate)
-        push!(tn, tensor)
-    end
-
-    for (lane, wireindices) in enumerate(wire)
-        for index in wireindices
-            tn.inds[index].meta[:site] = lane
-        end
-    end
-
-    for input in first.(wire)
-        tn.inds[input].meta[:plug] = :input
-    end
-
-    for output in last.(wire)
-        tn.inds[output].meta[:plug] = :output
-    end
-
-    return tn
-end
 
 function Base.hcat(A::TensorNetwork{QA}, B::TensorNetwork{QB}) where {QA<:Quantum,QB<:Quantum}
     outsites(A) != insites(B) && throw(DimensionMismatch("insites(B) must be equal to outsites(A) to connect them"))
