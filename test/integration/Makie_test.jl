6f3efdf5b21d3ba462e7265ef7e6f5e40831afaf
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+github@bsc.es>
2023-04-19 21:03:15 +0200

    Format code

--- test/integration/Makie_test.jl
+++ test/integration/Makie_test.jl
@@ -8,26 +8,26 @@
         tn = TensorNetwork([
             Tensor(rand(2, 2, 2, 2), (:x, :y, :z, :t)),
             Tensor(rand(2, 2), (:x, :y)),
-            Tensor(rand(2),(:x,))
-            ])
+            Tensor(rand(2), (:x,)),
+        ])
 
         @test plot(tn) isa FigureAxisPlot
-        @test plot(tn; labels=true) isa FigureAxisPlot
-        @test plot(tn; layout=Spring(dim=3)) isa FigureAxisPlot
+        @test plot(tn; labels = true) isa FigureAxisPlot
+        @test plot(tn; layout = Spring(dim = 3)) isa FigureAxisPlot
 
         @test begin
             f = Figure()
-            plot!(f[1,1], tn) isa AxisPlot
+            plot!(f[1, 1], tn) isa AxisPlot
         end
 
         @test begin
             f = Figure()
-            plot!(f[1,1], tn; labels=true) isa AxisPlot
+            plot!(f[1, 1], tn; labels = true) isa AxisPlot
         end
 
         @test begin
             f = Figure()
-            plot!(f[1,1], tn; layout=Spring(dim=3)) isa AxisPlot
+            plot!(f[1, 1], tn; layout = Spring(dim = 3)) isa AxisPlot
         end
     end
 end
