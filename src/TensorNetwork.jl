edc875d5606ce6ada59938fc4351e32f7c6cb606
Jofre <jofrevalles99@gmail.com>
2023-04-20 12:33:09 +0200

    Wrap replace functions together to resolve ambiguity

--- src/TensorNetwork.jl
+++ src/TensorNetwork.jl
@@ -45,16 +45,34 @@ function TensorNetwork{A}(tensors; meta...) where {A}
     return tn
 end
 
-Base.replace(tn::TensorNetwork, old_new::Pair{<:Tensor, <:Tensor}...) = replace!(copy(tn), old_new...)
+function Base.replace(tn::TensorNetwork, old_new::Pair...)
+    if all(x -> isa(x[1], Symbol) && isa(x[2], Symbol), old_new)
+        return replace_labels!(copy(tn), old_new...)
+    elseif all(x -> isa(x[1], Tensor) && isa(x[2], Tensor), old_new)
+        return replace_tensors!(copy(tn), old_new...)
+    else
+        throw(ArgumentError("The pairs must be either all (Symbol, Symbol) or all (Tensor, Tensor)"))
+    end
+end
 
-function Base.replace!(tn::TensorNetwork, pairs::Pair{<:Tensor, <:Tensor}...)
+function Base.replace!(tn::TensorNetwork, old_new::Pair...)
+    if all(x -> isa(x[1], Symbol) && isa(x[2], Symbol), old_new)
+        return replace_labels!(tn, old_new...)
+    elseif all(x -> isa(x[1], Tensor) && isa(x[2], Tensor), old_new)
+        return replace_tensors!(tn, old_new...)
+    else
+        throw(ArgumentError("The pairs must be either all (Symbol, Symbol) or all (Tensor, Tensor)"))
+    end
+end
+
+function replace_tensors!(tn::TensorNetwork, old_new::Pair{<:Tensor, <:Tensor}...)
     # Check if new tensors are already present in the network
-    new_tensors = [new_tensor for (_, new_tensor) in pairs]
+    new_tensors = [new_tensor for (_, new_tensor) in old_new]
     if !isdisjoint(new_tensors, tn.tensors)
         throw(ArgumentError("New tensors must not be already present in the network"))
     end
 
-    for (old_tensor, new_tensor) in pairs
+    for (old_tensor, new_tensor) in old_new
         # check if old and new tensors are compatible
         if !issetequal(labels(new_tensor), labels(old_tensor))
             throw(ArgumentError("New tensor labels do not match the existing tensor labels"))
@@ -232,9 +250,7 @@ end
 
 Base.delete!(tn::TensorNetwork, x) = (_ = pop!(tn, x); tn)
 
-Base.replace(tn::TensorNetwork, old_new::Pair{Symbol,Symbol}...) = replace!(copy(tn), old_new...)
-
-function Base.replace!(tn::TensorNetwork, old_new::Pair{Symbol,Symbol}...)
+function replace_labels!(tn::TensorNetwork, old_new::Pair{Symbol,Symbol}...)
     !isdisjoint(last.(old_new), labels(tn)) && throw(ArgumentError("target symbols must not be already present"))
 
     tensors = unique(Iterators.flatten([select(tn, i) for i in first.(old_new)]) |> collect)
