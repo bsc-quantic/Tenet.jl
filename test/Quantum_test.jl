6b798f65aa5ba99442deb94554580487e6b2fef7
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+github@bsc.es>
2023-05-05 01:47:45 +0200

    Fix tests

--- test/Quantum_test.jl
+++ test/Quantum_test.jl
@@ -14,8 +14,8 @@
         @test issetequal(sites(newtn), 1:2)
         @test issetequal(sites(newtn, :in), sites(tn, :in))
         @test issetequal(sites(newtn, :out), sites(tn, :out))
-        @test issetequal(nameof.(labels(newtn, :in)), nameof.(labels(tn, :in)))
-        @test issetequal(nameof.(labels(newtn, :out)), nameof.(labels(tn, :out)))
+        @test issetequal(labels(newtn, :in), labels(tn, :in))
+        @test issetequal(labels(newtn, :out), labels(tn, :out))
 
         # TODO @test_throws ErrorException ...
     end
