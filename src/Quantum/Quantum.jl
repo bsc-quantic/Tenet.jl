a4d4fe2021faa49118be933bece9e35a9de82226
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+github@bsc.es>
2023-05-07 10:54:42 +0200

    Fix ambiguity on `tensor`

--- src/Quantum/Quantum.jl
+++ src/Quantum/Quantum.jl
@@ -57,7 +57,7 @@ labels(tn::TensorNetwork, ::Val{:out}) = map(last, Iterators.filter((((_, dir), 
 labels(tn::TensorNetwork, ::Val{:out}, site) = tn[:plug][(site, :out)]
 labels(tn::TensorNetwork, ::Val{:virtual}) = setdiff(labels(tn, Val(:all)), labels(tn, Val(:plug)))
 
-tensors(tn::TensorNetwork{<:Quantum}, arg, args...) = tensors(plug(tn), tn, arg, args...)
+tensors(tn::TensorNetwork{<:Quantum}, site::Integer, args...) = tensors(plug(tn), tn, site, args...)
 tensors(::Type{State}, tn::TensorNetwork{<:Quantum}, site) = select(tn, labels(tn, :out, site)) |> only
 @valsplit 4 tensors(T::Type{Operator}, tn::TensorNetwork{<:Quantum}, site, dir::Symbol) =
     throw(MethodError(sites, "dir=$dir not recognized"))
