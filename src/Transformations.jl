612cd200a33c7b7535583dd86f75dff7e9c43797
Jofre <jofrevalles99@gmail.com>
2023-05-22 17:19:45 +0200

    Add DiagonalReduction transformation type

--- src/Transformations.jl
+++ src/Transformations.jl
@@ -2,15 +2,15 @@ using DeltaArrays
 
 abstract type Transformation end
 
-transform(tn::TensorNetwork, transformations) = (tn = copy(tn); transform!(tn, transformations); return tn)
+transform(tn::TensorNetwork, transformations; kwargs...) = (tn = copy(tn); transform!(tn, transformations; kwargs...); return tn)
 
 function transform! end
 
-transform!(tn::TensorNetwork, transformation::Type{<:Transformation}) = transform!(tn, transformation())
+transform!(tn::TensorNetwork, transformation::Type{<:Transformation}; kwargs...) = transform!(tn, transformation(); kwargs...)
 
-function transform!(tn::TensorNetwork, transformations)
+function transform!(tn::TensorNetwork, transformations; kwargs...)
     for transformation in transformations
-        transform!(tn, transformation)
+        transform!(tn, transformation; kwargs...)
     end
     return tn
 end
@@ -44,4 +44,63 @@ function transform!(tn::TensorNetwork, ::HyperindConverter)
     end
 end
 
-# TODO column reduction, diagonal reduction, rank simplification, split simplification
+struct DiagonalReduction <: Transformation end
+
+function transform!(tn::TensorNetwork, ::DiagonalReduction; output_inds=nothing, atol=1e-12)
+    output_inds = output_inds === nothing ? openinds(tn) : output_inds
+    queue = collect(keys(tn.tensors))
+    cache = Dict()
+
+    while !isempty(queue)
+        idx = pop!(queue)
+        tensor = tn.tensors[idx]
+        cache_key = ("dr", idx, objectid(tensor.data))
+
+        if haskey(cache, cache_key)
+            continue
+        end
+
+        diag_axes = find_diag_axes(tensor.data, atol)
+
+        if isempty(diag_axes)
+            cache[cache_key] = true
+            continue
+        end
+
+        for (i, j) in diag_axes
+            ix_i, ix_j = labels(tensor)[i], labels(tensor)[j]
+            new, old = (ix_j in output_inds) ? ((ix_i in output_inds) ? continue : (ix_j, ix_i)) : (ix_i, ix_j)
+
+            for other_idx in setdiff(keys(tn.tensors), idx)
+                other_tensor = tn.tensors[other_idx]
+                if old in labels(other_tensor)
+                    new_tensor = replace(other_tensor, old => new)
+                    tn.tensors[other_idx] = new_tensor
+                end
+            end
+
+            data = view(tensor, old => 1)
+            tn.tensors[idx] = Tensor(data, filter(l -> l != old, labels(tensor)))
+
+            !isempty(find_diag_axes(parent(tensor), atol)) && push!(queue, idx)
+        end
+    end
+
+    return tn
+end
+
+function find_diag_axes(x::AbstractArray, atol=1e-12)
+    ndims = size(x)
+
+    # Find all the potential diagonals
+    potential_diag_axes = [(i, j) for i in 1:length(ndims) for j in i+1:length(ndims) if ndims[i] == ndims[j]]
+
+    # Check what elements satisfy the condition
+    return filter(potential_diag_axes) do (d1, d2)
+        all(pairs(x)) do (idx, val)
+            idx[d1] == idx[d2] || abs(val) <= atol
+        end
+    end
+end
+
+# TODO column reduction, rank simplification, split simplification
