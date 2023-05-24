d923cb41c8a9605b12f6cb25d1138cfda5335b92
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+github@bsc.es>
2023-05-24 16:08:18 +0200

    Move `hcat` test to `Quac` integration tests

--- test/Quantum_test.jl
+++ test/Quantum_test.jl
@@ -1,22 +1,2 @@
 @testset "Quantum" begin
-    using Tenet: TensorNetwork, ansatz, Quantum, sites
-    using Quac
-
-    @testset "hcat" begin
-        n = 2
-        qft = Quac.Algorithms.QFT(n)
-        tn = TensorNetwork(qft)
-
-        newtn = hcat(tn, tn)
-
-        @test ansatz(newtn) <: Tuple{Quantum,Quantum}
-        # @test all(isphysical, inds(newtn))
-        @test issetequal(sites(newtn), 1:2)
-        @test issetequal(sites(newtn, :in), sites(tn, :in))
-        @test issetequal(sites(newtn, :out), sites(tn, :out))
-        @test issetequal(labels(newtn, :in), labels(tn, :in))
-        @test issetequal(labels(newtn, :out), labels(tn, :out))
-
-        # TODO @test_throws ErrorException ...
-    end
 end
--- test/integration/Quac_test.jl
+++ test/integration/Quac_test.jl
@@ -17,4 +17,22 @@
         # TODO `physicalinds`,`virtualinds`
         # @test all(isphysical, inds(tn))
     end
+
+    @testset "hcat" begin
+        n = 2
+        qft = Quac.Algorithms.QFT(n)
+        tn = TensorNetwork(qft)
+
+        newtn = hcat(tn, tn)
+
+        @test ansatz(newtn) <: Composite(Quantum, Quantum)
+        # @test all(isphysical, inds(newtn))
+        @test issetequal(sites(newtn), 1:2)
+        @test issetequal(sites(newtn, :in), sites(tn, :in))
+        @test issetequal(sites(newtn, :out), sites(tn, :out))
+        @test issetequal(labels(newtn, :in), labels(tn, :in))
+        @test issetequal(labels(newtn, :out), labels(tn, :out))
+
+        # TODO @test_throws ErrorException ...
+    end
 end
