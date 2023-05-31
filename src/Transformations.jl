96dc8d9d17b8e35f9bdc89f2bf971353afb43c37
Jofre <jofrevalles99@gmail.com>
2023-05-31 16:07:56 +0200

    Remove reduced label in indexs

--- src/Transformations.jl
+++ src/Transformations.jl
@@ -236,6 +236,7 @@ function transform!(tn::TensorNetwork, config::ColumnReduction)
                     tn.tensors[ind] = Tensor(view(parent(t), reduced_dims...), labels(t))
                 end
             end
+            delete!(tn.inds, ix_i)
         end
     end
 
