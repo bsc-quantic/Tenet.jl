140d653380db30c61a79197db0af48d4587f0a90
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2023-06-02 19:28:50 +0200

    Test `Quantum` code on `Operator` plug

--- test/Quantum_test.jl
+++ test/Quantum_test.jl
@@ -4,52 +4,118 @@
     struct MockState <: Quantum end
     Tenet.plug(::Type{MockState}) = State
 
-    tn = TensorNetwork{MockState}(
+    struct MockOperator <: Quantum end
+    Tenet.plug(::Type{MockOperator}) = Operator
+
+    state = TensorNetwork{MockState}(
         [Tensor(rand(2, 2), (:i, :k)), Tensor(rand(3, 2, 4), (:j, :k, :l))];
         plug = State,
         interlayer = [Bijection(Dict([1 => :i, 2 => :j]))],
     )
 
-    @testset "metadata" begin
-        @test fieldnames(Tenet.metadata(Quantum)) === (:plug, :interlayer)
-        @test Tenet.checkmeta(tn)
+    operator = TensorNetwork{MockOperator}(
+        [Tensor(rand(2, 4, 2), (:a, :c, :d)), Tensor(rand(3, 4, 2, 5), (:b, :c, :e, :f))];
+        plug = Operator,
+        interlayer = [Bijection(Dict([1 => :a, 2 => :b])), Bijection(Dict([1 => :d, 2 => :e]))],
+    )
 
-        @test hasproperty(tn, :interlayer)
-        @test only(tn.interlayer) == Bijection(Dict([1 => :i, 2 => :j]))
+    @testset "metadata" begin
+        @testset "State" begin
+            @test Tenet.checkmeta(state)
+            @test hasproperty(state, :interlayer)
+            @test only(state.interlayer) == Bijection(Dict([1 => :i, 2 => :j]))
+        end
+
+        @testset "Operator" begin
+            @test Tenet.checkmeta(operator)
+            @test hasproperty(operator, :interlayer)
+            @test operator.interlayer == [Bijection(Dict([1 => :a, 2 => :b])), Bijection(Dict([1 => :d, 2 => :e]))]
+        end
+    end
+
+    @testset "plug" begin
+        @test plug(state) === State
+
+        @test plug(operator) === Operator
     end
 
     # TODO write tests for
     # - boundary
-    # - plug
+    # - tensors
 
     @testset "sites" begin
-        @test issetequal(sites(tn), [1, 2])
+        @test issetequal(sites(state), [1, 2])
+        @test issetequal(sites(operator), [1, 2])
     end
 
     @testset "labels" begin
-        @test issetequal(labels(tn), [:i, :j, :k, :l])
-        @test issetequal(labels(tn, set = :open), [:i, :j, :l])
-        @test issetequal(labels(tn, set = :plug), [:i, :j])
-        @test issetequal(labels(tn, set = :inner), [:k])
-        @test isempty(labels(tn, set = :hyper))
-        @test issetequal(labels(tn, set = :virtual), [:k, :l])
+        @testset "State" begin
+            @test issetequal(labels(state), [:i, :j, :k, :l])
+            @test issetequal(labels(state, set = :open), [:i, :j, :l])
+            @test issetequal(labels(state, set = :plug), [:i, :j])
+            @test issetequal(labels(state, set = :inner), [:k])
+            @test isempty(labels(state, set = :hyper))
+            @test issetequal(labels(state, set = :virtual), [:k, :l])
+        end
+
+        # TODO change the indices
+        @testset "Operator" begin
+            @test issetequal(labels(operator), [:a, :b, :c, :d, :e, :f])
+            @test issetequal(labels(operator, set = :open), [:a, :b, :d, :e, :f])
+            @test issetequal(labels(operator, set = :plug), [:a, :b, :d, :e])
+            @test issetequal(labels(operator, set = :inner), [:c])
+            @test isempty(labels(operator, set = :hyper))
+            @test_broken issetequal(labels(operator, set = :virtual), [:c])
+        end
     end
 
-    # @testset "tensors" begin
-    #     @test tensors(tn) == tensors(tn, 1)
-    # end
-
     @testset "adjoint" begin
-        adj = adjoint(tn)
+        @testset "State" begin
+            adj = adjoint(state)
 
-        @test issetequal(sites(tn), sites(adj))
-        @test all(i -> labels(tn, :plug, i) == labels(adj, :plug, i), sites(tn))
+            @test issetequal(sites(state), sites(adj))
+            @test all(i -> labels(state, :plug, i) == labels(adj, :plug, i), sites(state))
+        end
+
+        @testset "Operator" begin
+            adj = adjoint(operator)
+
+            @test issetequal(sites(operator), sites(adj))
+            @test_broken all(i -> labels(operator, :plug, i) == labels(adj, :plug, i), sites(operator))
+            @test all(i -> first(operator.interlayer)[i] == last(adj.interlayer)[i], sites(operator))
+            @test all(i -> last(operator.interlayer)[i] == first(adj.interlayer)[i], sites(operator))
+        end
     end
 
     @testset "hcat" begin
-        let expectation = hcat(tn, tn')
-            @test issetequal(sites(expectation), sites(tn))
-            @test issetequal(labels(expectation, set = :plug), labels(tn, set = :plug))
+        @testset "(State, State)" begin
+            expectation = hcat(state, state)
+            @test issetequal(sites(expectation), sites(state))
+            @test issetequal(labels(expectation, set = :plug), labels(state, set = :plug))
+            @test isempty(labels(expectation, set = :open))
+            @test issetequal(labels(expectation, set = :inner), labels(expectation, set = :all))
+        end
+
+        @testset "(State, Operator)" begin
+            expectation = hcat(state, operator)
+            @test issetequal(sites(expectation), sites(state))
+            @test_broken issetequal(labels(expectation, set = :plug), labels(operator, set = :plug))
+            @test_broken isempty(labels(expectation, set = :open))
+            @test_broken issetequal(labels(expectation, set = :inner), labels(expectation, set = :all))
+        end
+
+        @testset "(Operator, State)" begin
+            expectation = hcat(operator, state)
+            @test issetequal(sites(expectation), sites(state))
+            @test_broken issetequal(labels(expectation, set = :plug), labels(state, set = :plug))
+            @test_broken isempty(labels(expectation, set = :open))
+            @test_broken issetequal(labels(expectation, set = :inner), labels(expectation, set = :all))
+        end
+
+        @testset "(Operator, Operator)" begin
+            expectation = hcat(operator, operator)
+            @test issetequal(sites(expectation), sites(state))
+            @test issetequal(labels(expectation, set = :plug), labels(operator, set = :plug))
             @test isempty(labels(expectation, set = :open))
             @test issetequal(labels(expectation, set = :inner), labels(expectation, set = :all))
         end
