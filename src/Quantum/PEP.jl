f1069a32151fa4aecb0fabd1cbc98255a3c50bb2
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2023-07-04 16:52:48 +0200

    Fix `ProjectedEntangledPair` constructor

--- src/Quantum/PEP.jl
+++ src/Quantum/PEP.jl
@@ -51,31 +51,34 @@ function ProjectedEntangledPair{P,B}(
     )
 
     m, n = size(arrays)
-    hinds = Dict((i, j) => Symbol(uuid4()) for i in 1:m for j in ringpeek(1:n))
-    vinds = Dict((i, j) => Symbol(uuid4()) for i in ringpeek(1:m) for j in 1:n)
-    oinds = Dict((i, j) => Symbol(uuid4()) for i in 1:m for j in 1:n)
-    iinds = Dict((i, j) => Symbol(uuid4()) for i in 1:m for j in 1:n)
+    hinds = Dict((i, j) => Symbol(uuid4()) for i in 1:m, j in ringpeek(1:n))
+    vinds = Dict((i, j) => Symbol(uuid4()) for i in ringpeek(1:m), j in 1:n)
+    oinds = Dict((i, j) => Symbol(uuid4()) for i in 1:m, j in 1:n)
+    iinds = Dict((i, j) => Symbol(uuid4()) for i in 1:m, j in 1:n)
 
     interlayer = if P <: State
-        [Bijection(oinds)]
+        [Bijection(Dict(i + j * m => index for ((i, j), index) in oinds))]
     elseif P <: Operator
-        [Bijection(iinds), Bijection(oinds)]
+        [
+            Bijection(Dict(i + j * m => index for ((i, j), index) in iinds)),
+            Bijection(Dict(i + j * m => index for ((i, j), index) in oinds)),
+        ]
     else
         throw(ErrorException("Plug $P is not valid"))
     end
 
-    tensors = map(zip(Iterators.map(Tuple, eachindex(arrays)), arrays)) do ((i, j), array)
+    tensors = map(zip(Iterators.map(Tuple, eachindex(IndexCartesian(), arrays)), arrays)) do ((i, j), array)
         dirs = _sitealias(ProjectedEntangledPair{P,B}, order, (m, n), (i, j))
 
         labels = map(dirs) do dir
             if dir === :l
-                hinds[((mod1(i - 1, n), i), j)]
+                hinds[(i, (mod1(j - 1, n), j))]
             elseif dir === :r
-                hinds[((i, mod1(i + 1, n)), j)]
+                hinds[(i, (j, mod1(j + 1, n)))]
             elseif dir === :u
-                vinds[(i, (mod1(j - 1, n), j))]
+                vinds[((mod1(i - 1, n), i), j)]
             elseif dir === :d
-                vinds[(i, (j, mod1(j + 1, n)))]
+                vinds[((i, mod1(i + 1, n)), j)]
             elseif dir === :i
                 iinds[(i, j)]
             elseif dir === :o
@@ -85,7 +88,7 @@ function ProjectedEntangledPair{P,B}(
         alias = Dict(dir => label for (dir, label) in zip(dirs, labels))
 
         Tensor(array, labels; alias = alias)
-    end
+    end |> vec
 
     return TensorNetwork{ProjectedEntangledPair{P,B}}(tensors; χ, plug = P, interlayer, metadata...)
 end
