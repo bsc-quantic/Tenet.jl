42c4e58fdfe99a1648d4fa50ef7a77df8453a5bd
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2023-09-10 20:54:19 +0200

    Replace `Zygote.@adjoint` rules for `ChainRules.rrule`s

--- ext/TenetChainRulesCoreExt.jl
+++ ext/TenetChainRulesCoreExt.jl
@@ -15,4 +15,25 @@ function ChainRulesCore.rrule(::Type{Tensor{T,N,A}}, data, inds; meta...) where 
     end
 end
 
+function ChainRulesCore.rrule(T::Type{<:Tensor}, data, inds; meta...)
+    Tensor_pullback(Δ) = (NoTangent(), Δ.data, NoTangent())
+    return T(data, inds; meta...), Tensor_pullback
+end
+
+# WARN type-piracy
+function ChainRulesCore.rrule(::typeof(setdiff), s, itrs...)
+    setdiff_pullback(_) = fill(NoTangent(), 2 + length(itrs))
+    return setdiff(s, itrs...), setdiff_pullback
+end
+
+function ChainRulesCore.rrule(::typeof(union), s, itrs...)
+    union_pullback(_) = fill(NoTangent(), 2 + length(itrs))
+    return union(s, itrs...), union_pullback
+end
+
+function ChainRulesCore.rrule(::typeof(intersect), s, itrs...)
+    intersect_pullback(_) = fill(NoTangent(), 2 + length(itrs))
+    return intersect(s, itrs...), intersect_pullback
+end
+
 end
