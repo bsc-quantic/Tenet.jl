2089a76b28e037de70187b675585d242b35aa447
Jofre Vallès Muns <61060572+jofrevalles@users.noreply.github.com>
2023-10-31 14:22:43 +0100

    Enhance `qr` function with `mode` argument (#114)
    * Add mode argument into qr function
    
    * Remove mode argument, default to reduced QR
    
    * Fix test
    
    * Remove unnecessary argument

--- src/Numerics.jl
+++ src/Numerics.jl
@@ -119,8 +119,17 @@ end
 
 LinearAlgebra.qr(t::Tensor{<:Any,2}; kwargs...) = Base.@invoke qr(t::Tensor; left_inds = (first(inds(t)),), kwargs...)
 
-function LinearAlgebra.qr(t::Tensor; left_inds = (), right_inds = (), virtualind::Symbol = Symbol(uuid4()), kwargs...)
-    isdisjoint(left_inds, right_inds) ||
+"""
+    LinearAlgebra.qr(t::Tensor, mode::Symbol = :reduced; left_inds = (), right_inds = (), virtualind::Symbol = Symbol(uuid4()), kwargs...
+Perform QR factorization on a tensor.
+# Arguments
+    - `t::Tensor`: tensor to be factorized
+# Keyword Arguments
+    - `left_inds`: left indices to be used in the QR factorization. Defaults to all indices of `t` except `right_inds`.
+    - `right_inds`: right indices to be used in the QR factorization. Defaults to all indices of `t` except `left_inds`.
+    - `virtualind`: name of the virtual bond. Defaults to a random `Symbol`.
+"""
+function LinearAlgebra.qr(t::Tensor; left_inds = (), right_inds = (), virtualind::Symbol = Symbol(uuid4()), kwargs...)    isdisjoint(left_inds, right_inds) ||
         throw(ArgumentError("left ($left_inds) and right $(right_inds) indices must be disjoint"))
 
     left_inds, right_inds =
@@ -138,7 +147,8 @@ function LinearAlgebra.qr(t::Tensor; left_inds = (), right_inds = (), virtualind
     data = reshape(parent(tensor), prod(i -> size(t, i), left_inds), prod(i -> size(t, i), right_inds))
 
     # compute QR
-    Q, R = qr(data; kwargs...)
+    F = qr(data; kwargs...)
+    Q, R = Matrix(F.Q), Matrix(F.R)
 
     # tensorify results
     Q = reshape(Q, ([size(t, ind) for ind in left_inds]..., size(Q, 2)))
