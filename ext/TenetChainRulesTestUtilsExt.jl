7442a7bb4b43bf2a3888f9dfc22c4db46804a514
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2023-11-06 13:48:58 +0100

    Refactor `ChainRulesTestUtils.rand_tangent` to new `TensorNetwork` fields

--- ext/TenetChainRulesTestUtilsExt.jl
+++ ext/TenetChainRulesTestUtilsExt.jl
@@ -7,7 +7,10 @@ using ChainRulesTestUtils
 using Random
 
 function ChainRulesTestUtils.rand_tangent(rng::AbstractRNG, x::T) where {T<:AbstractTensorNetwork}
-    return Tangent{T}(tensors = [ProjectTo(tensor)(rand_tangent.(Ref(rng), tensor)) for tensor in tensors(x)])
+    return Tangent{T}(
+        tensormap = [ProjectTo(tensor)(rand_tangent.(Ref(rng), tensor)) for tensor in tensors(x)],
+        indexmap = NoTangent(),
+    )
 end
 
 end
