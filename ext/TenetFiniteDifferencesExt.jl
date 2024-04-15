9f63544ff2af6d605fb194708901cc250df317a6
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-04-15 21:04:06 +0200

    Fix vectorization of `Tensor` dictionary in `TensorNetworkTangent`
    Order wasn't preserved which lead to wrong gradients

--- ext/TenetFiniteDifferencesExt.jl
+++ ext/TenetFiniteDifferencesExt.jl
@@ -10,4 +10,14 @@ function FiniteDifferences.to_vec(x::TensorNetwork)
     return x_vec, TensorNetwork_from_vec
 end
 
+# fixes wrong tangents on `TensorNetworkTangent` objects
+# see: "different ndims" test in `ChainRules_test.jl`
+function FiniteDifferences.to_vec(x::Dict{Vector{Symbol},Tensor})
+    tensors = sort!(collect(values(x)), by = inds)
+    x_vec, back = to_vec(tensors)
+    Dict_from_vec(v) = Dict(zip(inds.(tensors), back(v)))
+
+    return x_vec, Dict_from_vec
+end
+
 end
