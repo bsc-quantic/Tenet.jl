c2b7ff366f27216cdf5e482f42ebbbd39d681010
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-04-15 20:58:34 +0200

    Fix `rand_tangent` for empty `Vector{Tensor}`

--- ext/TenetChainRulesTestUtilsExt.jl
+++ ext/TenetChainRulesTestUtilsExt.jl
@@ -7,6 +7,14 @@ using Random
 
 const TensorNetworkTangent = Base.get_extension(Tenet, :TenetChainRulesCoreExt).TensorNetworkTangent
 
+function ChainRulesTestUtils.rand_tangent(rng::AbstractRNG, x::Vector{T}) where {T<:Tensor}
+    if isempty(x)
+        return Vector{T}()
+    else
+        @invoke rand_tangent(rng::AbstractRNG, x::AbstractArray)
+    end
+end
+
 function ChainRulesTestUtils.rand_tangent(rng::AbstractRNG, x::TensorNetwork)
     return TensorNetworkTangent(Tensor[ProjectTo(tensor)(rand_tangent.(Ref(rng), tensor)) for tensor in tensors(x)])
 end
