a60cd01f6cea9714781a2d991831da9ea8c9a4cb
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-04-30 19:00:44 +0200

    Fix typo in `GraphMakie` extension

--- ext/TenetGraphMakieExt.jl
+++ ext/TenetGraphMakieExt.jl
@@ -7,9 +7,9 @@ using Tenet
 using Combinatorics: combinations
 
 """
-    plot(tn::TensorNetwork; kwargs...)
-    plot!(f::Union{Figure,GridPosition}, tn::TensorNetwork; kwargs...)
-    plot!(ax::Union{Axis,Axis3}, tn::TensorNetwork; kwargs...)
+    graphplot(tn::TensorNetwork; kwargs...)
+    graphplot!(f::Union{Figure,GridPosition}, tn::TensorNetwork; kwargs...)
+    graphplot!(ax::Union{Axis,Axis3}, tn::TensorNetwork; kwargs...)
 
 Plot a [`TensorNetwork`](@ref) as a graph.
 
@@ -39,7 +39,7 @@ function GraphMakie.graphplot!(f::Union{Figure,GridPosition}, tn::TensorNetwork;
     hidedecorations!(ax)
     hidespines!(ax)
 
-    p = plot!(ax, tn; kwargs...)
+    p = graphplot!(ax, tn; kwargs...)
 
     return Makie.AxisPlot(ax, p)
 end
@@ -51,7 +51,7 @@ function GraphMakie.graphplot!(ax::Union{Axis,Axis3}, tn::TensorNetwork; labels=
     tensormap = IdDict(tensor => i for (i, tensor) in enumerate(tensors(tn)))
 
     graph = Graphs.SimpleGraph(length(tensors(tn)))
-    for i in setdiff(inds(tn, :inner), inds(tn, :hyper))
+    for i in setdiff(inds(tn; set=:inner), inds(tn; set=:hyper))
         edge_tensors = tensors(tn, :any, i)
 
         @assert length(edge_tensors) == 2
@@ -62,7 +62,7 @@ function GraphMakie.graphplot!(ax::Union{Axis,Axis3}, tn::TensorNetwork; labels=
 
     # TODO recognise `copytensors` by using `DeltaArray` or `Diagonal` representations
     copytensors = findall(tensor -> any(flatinds -> issetequal(inds(tensor), flatinds), keys(hypermap)), tensors(tn))
-    ghostnodes = map(inds(tn, :open)) do index
+    ghostnodes = map(inds(tn; set=:open)) do index
         # create new ghost node
         Graphs.add_vertex!(graph)
         node = Graphs.nv(graph)
@@ -104,14 +104,14 @@ function GraphMakie.graphplot!(ax::Union{Axis,Axis3}, tn::TensorNetwork; labels=
 
     # configure labels
     labels == true && get!(kwargs, :elabels) do
-        opentensors = findall(t -> !isdisjoint(inds(t), inds(tn, :open)), tensors(tn))
+        opentensors = findall(t -> !isdisjoint(inds(t), inds(tn; set=:open)), tensors(tn))
         opencounter = IdDict(tensor => 0 for tensor in opentensors)
 
         map(Graphs.edges(graph)) do edge
             # case: open edge
             if any(∈(ghostnodes), [Graphs.src(edge), Graphs.dst(edge)])
                 notghost = Graphs.src(edge) ∈ ghostnodes ? Graphs.dst(edge) : Graphs.src(edge)
-                inds = Tenet.inds(tn, :open) ∩ Tenet.inds(tensors(tn)[notghost])
+                inds = Tenet.inds(tn; set=:open) ∩ Tenet.inds(tensors(tn)[notghost])
                 opencounter[notghost] += 1
                 return string(inds[opencounter[notghost]])
             end
