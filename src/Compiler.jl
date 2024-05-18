b28ec7cb32d1b6f51249579d8fb4796adbac4668
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-05-18 17:31:48 +0200

    Refactor `compile`

--- src/Compiler.jl
+++ src/Compiler.jl
@@ -1,44 +1,35 @@
 using EinExprs
-using OMEinsum
 using AbstractTrees
 
 function compile(path::EinExpr)
-    ssa = IdDict(leave => i for (i, leave) in enumerate(Leaves(path)))
+    ssa = IdDict(leave => i for (i, leave) in enumerate(Iterators.flatten([Leaves(path), Branches(path)])))
 
-    ssa_leaves = map(Leaves(path)) do leave
-        i = ssa[leave]
-        :($(Symbol(:ssa, i)) = parent(getindex(tn, $(head(leave))...)))
+    args = map(Iterators.flatten([Leaves(path), Branches(path)])) do node
+        i = ssa[node]
+        N = ndims(node)
+        :($(Symbol(:ssa, i))) #::Tensor{$T,$N})
     end
 
     ssa_eincodes = map(Branches(path)) do branch
         @assert EinExprs.nargs(branch) == 2 "Only binary contractions are supported"
 
-        a, b = args(branch)
+        a, b = EinExprs.args(branch)
         i, j = ssa[a], ssa[b]
 
         ssa_a = Symbol(:ssa, i)
         ssa_b = Symbol(:ssa, j)
 
-        k = length(ssa) + 1
+        k = ssa[branch]
         ssa_c = Symbol(:ssa, k)
 
         ssa[branch] = k
 
-        ixs = (tuple(head(a)...), tuple(head(b)...))
-        iy = tuple(head(branch)...)
-        eincode = StaticEinCode{Symbol,ixs,iy}()
-
-        :($ssa_c = $eincode($ssa_a, $ssa_b))
+        # WARN hardcoded return type
+        return :(contract!($ssa_c, $ssa_a, $ssa_b))
     end
 
-    :(function $(gensym(:contract_compiled))(tn::TensorNetwork)
-        # initialize SSA with leaves
-        $(ssa_leaves...)
-
-        # compute branches
+    :(function $(gensym(:contract_compiled))($(args...)) # FIX this is hardcoded
         $(ssa_eincodes...)
-
-        # return Tensor(Zygote.hook(c̄ -> @show(c̄), $(Symbol(:ssa, ssa[path]))), $(head(path)))
-        return Tensor($(Symbol(:ssa, ssa[path])), $(head(path)))
+        return $(Symbol(:ssa, ssa[path]))
     end)
 end
