ded04fe1323d2a2967ff02fa72e34af93761a5dc
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-05-19 12:48:59 +0200

    Fix generated methods by `codegen`

--- src/Compiler.jl
+++ src/Compiler.jl
@@ -50,6 +50,11 @@ function codegen(::Val{:outplace}, path::EinExpr)
         :($(Symbol(:ssa, i)))
     end
 
+    tensorargs = map(Leaves(path)) do node
+        i = ssa[node]
+        :($(Symbol(:ssa, i))::Tensor)
+    end
+
     ssa_eincodes = map(Branches(path)) do branch
         @assert EinExprs.nargs(branch) == 2 "Only binary contractions are supported"
 
@@ -67,8 +72,22 @@ function codegen(::Val{:outplace}, path::EinExpr)
         return :($ssa_c = contract($ssa_a, $ssa_b))
     end
 
-    :(function $(gensym(:contract_compiled))($(args...))
-        $(ssa_eincodes...)
-        return $(Symbol(:ssa, ssa[path]))
-    end)
+    fname = gensym(:contract_compiled)
+
+    quote
+        function $fname($(tensorargs...))
+            $(ssa_eincodes...)
+            return $(Symbol(:ssa, ssa[path]))
+        end
+
+        function $fname($(args...))
+            $(map(Iterators.flatten([Leaves(path), Branches(path)])) do node
+                i = ssa[node]
+                name = Symbol(:ssa, i)
+                :($name = Tensor($name, $(head(node))))
+            end...)
+
+            return $fname($(args...))
+        end
+    end
 end
