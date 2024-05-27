b7e7aa2f7b572d6a258540977f3a1a9a64d583e0
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2024-05-27 21:25:53 +0200

    Implement `codegen` method for computing the gradient

--- src/Compiler.jl
+++ src/Compiler.jl
@@ -106,6 +106,97 @@ function codegen(::Val{:outplace}, path::EinExpr)
     end
 end
 
+function codegen(::Val{:grad}, path::EinExpr)
+    ssa = IdDict(leave => i for (i, leave) in enumerate(Iterators.flatten([Leaves(path), Branches(path)])))
+
+    args = map(Leaves(path)) do node
+        i = ssa[node]
+        :($(Symbol(:ssa, i)))
+    end
+
+    tensorargs = map(Leaves(path)) do node
+        i = ssa[node]
+        :($(Symbol(:ssa, i))::Tensor)
+    end
+
+    ssa_primal_eincodes = map(Branches(path)) do branch
+        @assert EinExprs.nargs(branch) == 2 "Only binary contractions are supported"
+
+        a, b = EinExprs.args(branch)
+        i, j = ssa[a], ssa[b]
+
+        ssa_a = Symbol(:ssa, i)
+        ssa_b = Symbol(:ssa, j)
+
+        k = ssa[branch]
+        ssa_c = Symbol(:ssa, k)
+
+        ssa[branch] = k
+
+        return :($ssa_c = contract($ssa_a, $ssa_b))
+    end
+
+    ssa_reverse_eincodes = map(Branches(path; inverse=true)) do branch
+        @assert EinExprs.nargs(branch) == 2 "Only binary contractions are supported"
+
+        a, b = EinExprs.args(branch)
+        i, j = ssa[a], ssa[b]
+
+        ssa_a = Symbol(:ssa, i)
+        ssa_b = Symbol(:ssa, j)
+        ∇ssa_a = Symbol(:∇ssa, i)
+        ∇ssa_b = Symbol(:∇ssa, j)
+
+        k = ssa[branch]
+        ∇ssa_c = Symbol(:∇ssa, k)
+
+        return quote
+            $∇ssa_a = contract($∇ssa_c, conj($ssa_b); out=inds($ssa_a))
+            $∇ssa_b = contract($∇ssa_c, conj($ssa_a); out=inds($ssa_b))
+        end
+    end
+
+    fname = gensym(:∇contract)
+
+    code_init_tangent = let k = ssa[path]
+        ssa_c = Symbol(:ssa, k)
+        ∇ssa_c = Symbol(:∇ssa, k)
+
+        quote
+            $∇ssa_c = similar($ssa_c)
+            $∇ssa_c[] = one(eltype($∇ssa_c))
+        end
+    end
+
+    quote
+        function $fname($(tensorargs...))
+            # primal computation
+            $(ssa_primal_eincodes...)
+
+            # TODO set tangent of the output before backpropagation
+            $code_init_tangent
+
+            # reverse pass
+            $(ssa_reverse_eincodes...)
+
+            return (;
+                primal=$(Symbol(:ssa, ssa[path])),
+                grad=($(map(((i, _),) -> Symbol(:∇ssa, i), enumerate(Leaves(path)))...),),
+            )
+        end
+
+        function $fname($(args...))
+            $(map(Leaves(path)) do node
+                i = ssa[node]
+                name = Symbol(:ssa, i)
+                :($name = Tensor($name, $(head(node))))
+            end...)
+
+            return $fname($(args...))
+        end
+    end
+end
+
 function codegen(::Val{:TensorNetwork_from_arrays}, tn::TensorNetwork)
     args = map(1:ntensors(tn)) do i
         Symbol(:arg_, i)
