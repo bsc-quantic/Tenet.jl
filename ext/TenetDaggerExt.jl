6d0e36c102d93c67a68abc7d5db85132a4608aa4
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-05-29 17:31:54 +0200

    Format code

--- ext/TenetDaggerExt.jl
+++ ext/TenetDaggerExt.jl
@@ -14,30 +14,32 @@ struct Contract{T,N} <: ArrayOp{T,N}
         allunique(ia) || throw(ErrorException("ia must have unique indices"))
         allunique(ib) || throw(ErrorException("ib must have unique indices"))
         allunique(ic) || throw(ErrorException("ic must have unique indices"))
-        new{promote_type(eltype(a), eltype(b)),length(ic)}(a, ia, b, ib, ic)
+        ic ⊆ ia ∪ ib || throw(ErrorException("ic must be a subset of ia ∪ ib"))
+        return new{promote_type(eltype(a), eltype(b)),length(ic)}(a, ia, b, ib, ic)
     end
 end
 
-Base.size(x::Contract) = map(x.ic) do i
-    i ∈ x.ia && return size(x.a, findfirst(==(i), x.ia))
-    i ∈ x.ib && return size(x.b, findfirst(==(i), x.ib))
-    throw(ErrorException("index $i not found in a nor b"))
-end |> splat(tuple)
+function Base.size(x::Contract)
+    return Tuple(i ∈ x.ia ? size(x.a, findfirst(==(i), x.ia)) : size(x.b, findfirst(==(i), x.ib) for i in x.ic))
+end
 
-Dagger.Blocks(x::Contract) = map(x.ic) do i
-    j = findfirst(==(i), x.ia)
-    isnothing(j) || return x.a.partitioning[j]
+function Dagger.Blocks(x::Contract)
+    return Dagger.Blocks(map(x.ic) do i
+        j = findfirst(==(i), x.ia)
+        isnothing(j) || return x.a.partitioning[j]
 
-    j = findfirst(==(i), x.ia)
-    isnothing(j) || return x.b.partitioning[j]
+        j = findfirst(==(i), x.ia)
+        isnothing(j) || return x.b.partitioning[j]
 
-    throw(ErrorException("index $i not found in a nor b"))
-end |> splat(Dagger.Blocks)
+        throw(ErrorException("index $i not found in a nor b"))
+    end...)
+end
 
-selectdims(a, proj::Pair...) =
-    reduce(proj; init = a) do acc, (d, i)
+function selectdims(a, proj::Pair...)
+    reduce(proj; init=a) do acc, (d, i)
         selectdim(acc, d, i)
     end
+end
 
 function Dagger.stage(ctx::Context, op::Contract{T,N}) where {T,N}
     domain = Dagger.ArrayDomain([1:l for l in size(op)])
@@ -46,13 +48,16 @@ function Dagger.stage(ctx::Context, op::Contract{T,N}) where {T,N}
     # NOTE careful with ÷ for dividing into partitions
     subdomains = Array{ArrayDomain{N}}(undef, map(÷, size(op), partitioning.blocksize))
     for indices in eachindex(IndexCartesian(), subdomains)
-        subdomains[indices...] = map(indices, partitioning.blocksize) do i, step
-            (i - 1) * step .+ (1:step)
-        end |> ArrayDomain
+        subdomains[indices...] = ArrayDomain(
+            map(indices, partitioning.blocksize) do i, step
+                (i - 1) * step .+ (1:step)
+            end,
+        )
     end
 
-    contractor =
-        EinCode((Tenet.__omeinsum_sym2str(op.ia), Tenet.__omeinsum_sym2str(op.ib)), Tenet.__omeinsum_sym2str(op.ic))
+    contractor = EinCode(
+        (Tenet.__omeinsum_sym2str(op.ia), Tenet.__omeinsum_sym2str(op.ib)), Tenet.__omeinsum_sym2str(op.ic)
+    )
 
     suminds = setdiff(op.ia ∪ op.ib, op.ic)
     inner_perm_a = map(i -> findfirst(==(i), op.ia), suminds)
@@ -66,32 +71,33 @@ function Dagger.stage(ctx::Context, op::Contract{T,N}) where {T,N}
     chunks = similar(subdomains, EagerThunk)
     for indices in eachindex(IndexCartesian(), chunks)
         outer_indices_a = indices[mask_a]
-        chunks_a = reduce(zip(outer_perm_a, outer_indices_a); init = Dagger.chunks(op.a)) do acc, (d, i)
+        chunks_a = reduce(zip(outer_perm_a, outer_indices_a); init=Dagger.chunks(op.a)) do acc, (d, i)
             selectdim(acc, d, i:i)
         end
         chunks_a = permutedims(chunks_a, inner_perm_a)
 
         outer_indices_b = indices[mask_b]
-        chunks_b = reduce(zip(outer_perm_b, outer_indices_b); init = Dagger.chunks(op.b)) do acc, (d, i)
+        chunks_b = reduce(zip(outer_perm_b, outer_indices_b); init=Dagger.chunks(op.b)) do acc, (d, i)
             selectdim(acc, d, i:i)
         end
         chunks_b = permutedims(chunks_b, inner_perm_b)
 
-        chunks[indices...] = Dagger.treereduce(Dagger.AddComputeOp, map(chunks_a, chunks_b) do chunk_a, chunk_b
-            Dagger.@spawn contractor(chunk_a, chunk_b)
-        end)
+        chunks[indices...] = Dagger.treereduce(
+            Dagger.AddComputeOp,
+            map(chunks_a, chunks_b) do chunk_a, chunk_b
+                Dagger.@spawn contractor(chunk_a, chunk_b)
+            end,
+        )
     end
 
-    DArray(T, domain, subdomains, chunks, partitioning)
+    return DArray(T, domain, subdomains, chunks, partitioning)
 end
 
 function Tenet.contract(
-    a::Tensor{Ta,Na,Aa},
-    b::Tensor{Tb,Nb,Ab};
-    dims = (∩(inds(a), inds(b))),
+    a::Tensor{Ta,Na,Aa}, b::Tensor{Tb,Nb,Ab}; dims=(∩(inds(a), inds(b)))
 ) where {Ta,Tb,Na,Nb,Aa<:Dagger.DArray{Ta,Na},Ab<:Dagger.DArray{Tb,Nb}}
-    ia = inds(a) |> collect
-    ib = inds(b) |> collect
+    ia = collect(inds(a))
+    ib = collect(inds(b))
     i = ∩(dims, ia, ib)
 
     ic = setdiff(ia ∪ ib, i isa Base.AbstractVecOrTuple ? i : (i,))::Vector{Symbol}
