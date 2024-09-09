4d61124da955a8779a8747c50887fb4ae6c93296
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-09-09 17:10:29 -0400

    Support conversion from `ITensor` to `Tensor`

--- ext/TenetITensorsExt.jl
+++ ext/TenetITensorsExt.jl
@@ -1,6 +1,12 @@
 module TenetITensorsExt
 
 using Tenet
-using ITensors
+using ITensors: ITensors, ITensor
+
+function Tenet.Tensor(tensor::ITensor)
+    array = ITensors.array(tensor)
+    is = Symbol.(id.(ITensor.inds(tensor)))
+    return Tensor(array, is)
+end
 
 end
