eaf36eadf8b4320070e67a26f12734f743a6146b
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-09-10 11:52:45 -0400

    Support conversion from `ITensorNetwork` to `Quantum`, `TensorNetwork`

--- ext/TenetITensorNetworksExt.jl
+++ ext/TenetITensorNetworksExt.jl
@@ -1,6 +1,22 @@
 module TenetITensorNetworksExt
 
 using Tenet
-using ITensorNetworks
+using ITensorNetworks: ITensorNetworks, ITensorNetwork, ITensor, siteinds, plev, vertices
+const ITensors = ITensorNetworks.ITensors
+const DataGraphs = ITensorNetworks.DataGraphs
+
+Tenet.TensorNetwork(tn::ITensorNetwork) = TensorNetwork([tn[v] for v in vertices(tn)])
+
+function Tenet.Quantum(tn::ITensorNetwork)
+    sitedict = Dict(
+        map(pairs(DataGraphs.vertex_data(siteinds(tn)))) do (loc, index)
+            index = only(index)
+            primelevel = plev(index)
+            @assert primelevel ∈ (0, 1)
+            Site(loc; dual=Bool(primelevel)) => Symbol(ITensors.id(index))
+        end,
+    )
+    return Quantum(TensorNetwork(tn), sitedict)
+end
 
 end
