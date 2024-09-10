29d69d2ad732f70fabf46c3ca5d6631e460d2b71
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-09-10 17:13:30 -0400

    Fix some typos and problems

--- ext/TenetITensorNetworksExt.jl
+++ ext/TenetITensorNetworksExt.jl
@@ -4,11 +4,12 @@ using Tenet
 using ITensorNetworks: ITensorNetworks, ITensorNetwork, ITensor, Index, siteinds, plev, vertices
 const ITensors = ITensorNetworks.ITensors
 const DataGraphs = ITensorNetworks.DataGraphs
+const TenetITensorsExt = Base.get_extension(Tenet, :TenetITensorsExt)
 
 Base.convert(::Type{TensorNetwork}, tn::ITensorNetwork) = TensorNetwork([convert(Tensor, tn[v]) for v in vertices(tn)])
 
-function Base.convert(::Type{ITensorNetwork}, tn::AbstractTensorNetwork; inds=Dict{Symbol,Index}())
-    return ITensorNetwork(convert(Vector{ITensor}, tn); inds)
+function Base.convert(::Type{ITensorNetwork}, tn::Tenet.AbstractTensorNetwork; inds=Dict{Symbol,Index}())
+    return ITensorNetwork(convert(Vector{ITensor}, tn; inds))
 end
 
 function Base.convert(::Type{Quantum}, tn::ITensorNetwork)
@@ -17,7 +18,10 @@ function Base.convert(::Type{Quantum}, tn::ITensorNetwork)
             index = only(index)
             primelevel = plev(index)
             @assert primelevel ∈ (0, 1)
-            Site(loc; dual=Bool(primelevel)) => Symbol(ITensors.id(index))
+
+            # NOTE ITensors' Index's tag only has space for 16 characters
+            tag = ITensors.id(index)
+            Site(loc; dual=Bool(primelevel)) => TenetITensorsExt.symbolize(index)
         end,
     )
     return Quantum(convert(TensorNetwork, tn), sitedict)
--- ext/TenetITensorsExt.jl
+++ ext/TenetITensorsExt.jl
@@ -3,15 +3,29 @@ module TenetITensorsExt
 using Tenet
 using ITensors: ITensors, ITensor, Index
 
+function symbolize(index::Index)
+    tag = string(ITensors.id(index))
+
+    # NOTE ITensors' Index's tag only has space for 16 characters
+    return Symbol(length(tag) > 16 ? tag[(end - 16 + 1):end] : tag)
+end
+
+function tagize(index::Symbol)
+    tag = string(index)
+
+    # NOTE ITensors' Index's tag only has space for 16 characters
+    return length(tag) > 16 ? tag[(end - 16 + 1):end] : tag
+end
+
 function Base.convert(::Type{Tensor}, tensor::ITensor)
     array = ITensors.array(tensor)
-    is = Symbol.(ITensors.id.(ITensors.inds(tensor)))
+    is = map(symbolize, ITensors.inds(tensor))
     return Tensor(array, is)
 end
 
 function Base.convert(::Type{ITensor}, tensor::Tensor; inds=Dict{Symbol,Index}())
     indices = map(Tenet.inds(tensor)) do i
-        haskey(inds, i) ? inds[i] : Index(size(tensor, i), string(i))
+        haskey(inds, i) ? inds[i] : Index(size(tensor, i), tagize(i))
     end
     return ITensor(parent(tensor), indices)
 end
@@ -20,8 +34,8 @@ Base.convert(::Type{TensorNetwork}, tn::Vector{ITensor}) = TensorNetwork(map(Ten
 
 function Base.convert(::Type{Vector{ITensor}}, tn::Tenet.AbstractTensorNetwork; inds=Dict{Symbol,Index}())
     indices = merge(inds, Dict(
-        map(Iterators.filter(Base.Fix1(haskey, inds), Tenet.inds(tn))) do i
-            i => Index(size(tn, i), string(i))
+        map(Iterators.filter(!Base.Fix1(haskey, inds), Tenet.inds(tn))) do i
+            i => Index(size(tn, i), tagize(i))
         end,
     ))
     return map(tensors(tn)) do tensor
