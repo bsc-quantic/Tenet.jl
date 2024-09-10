6a6e0cd9ef3606395b7a1828f472943dac88a1e7
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-09-10 12:31:38 -0400

    Support conversion from `Tensor`, `TensorNetwork` to `ITensor`, `Vector{ITensor}`

--- ext/TenetITensorsExt.jl
+++ ext/TenetITensorsExt.jl
@@ -1,7 +1,7 @@
 module TenetITensorsExt
 
 using Tenet
-using ITensors: ITensors, ITensor
+using ITensors: ITensors, ITensor, Index
 
 function Tenet.Tensor(tensor::ITensor)
     array = ITensors.array(tensor)
@@ -9,6 +9,24 @@ function Tenet.Tensor(tensor::ITensor)
     return Tensor(array, is)
 end
 
+function Base.convert(::Type{ITensor}, tensor::Tensor; inds=Dict{Symbol,Index}())
+    indices = map(Tenet.inds(tensor)) do i
+        haskey(inds, i) ? inds[i] : Index(size(tensor, i), string(i))
+    end
+    return ITensor(parent(tensor), indices)
+end
+
 Tenet.TensorNetwork(tn::Vector{ITensor}) = TensorNetwork(map(Tensor, tn))
+
+function Base.convert(::Type{Vector{ITensor}}, tn::Tenet.AbstractTensorNetwork; inds=Dict{Symbol,Index}())
+    indices = merge(inds, Dict(
+        map(Iterators.filter(Base.Fix1(haskey, inds), Tenet.inds(tn))) do i
+            i => Index(size(tn, i), string(i))
+        end,
+    ))
+    return map(tensors(tn)) do tensor
+        ITensor(parent(tensor), map(i -> indices[i], Tenet.inds(tensor)))
+    end
+end
 
 end
