aadace7c598fe2a9cf389daafa0dfc1e03dedf7d
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-09-12 06:00:48 -0400

    Implement `rand`, `adjoint`, `defaultorder`, `boundary`, `form` for `MPS`, `MPO`

--- src/Ansatz/MPS.jl
+++ src/Ansatz/MPS.jl
@@ -1,44 +1,84 @@
-struct MPS <: Ansatz
+using Random
+
+abstract type AbstractMPS <: Ansatz end
+
+struct MPS <: AbstractMPS
     super::Quantum
+    form::Form
 end
+
+defaultorder(::Type{MPS}) = (:o, :l, :r)
+boundary(::MPS) = Open()
+form(tn::MPS) = tn.form
 
 function MPS(arrays::Vector{<:AbstractArray}; order=defaultorder(MPS))
     @assert ndims(arrays[1]) == 2 "First array must have 2 dimensions"
     @assert all(==(3) ∘ ndims, arrays[2:(end - 1)]) "All arrays must have 3 dimensions"
     @assert ndims(arrays[end]) == 2 "Last array must have 2 dimensions"
-    issetequal(order, defaultorder(Chain, State())) ||
-        throw(ArgumentError("order must be a permutation of $(String.(defaultorder(Chain, State())))"))
+    issetequal(order, defaultorder(MPS)) ||
+        throw(ArgumentError("order must be a permutation of $(String.(defaultorder(MPS)))"))
 
     n = length(arrays)
     gen = IndexCounter()
     symbols = [nextindex!(gen) for _ in 1:(2n)]
 
-    _tensors = map(enumerate(arrays)) do (i, array)
-        _order = if i == 1
-            filter(x -> x != :l, order)
-        elseif i == n
-            filter(x -> x != :r, order)
-        else
-            order
-        end
-
-        inds = map(_order) do dir
-            if dir == :o
-                symbols[i]
-            elseif dir == :r
-                symbols[n + mod1(i, n)]
-            elseif dir == :l
-                symbols[n + mod1(i - 1, n)]
+    tn = TensorNetwork(
+        map(enumerate(arrays)) do (i, array)
+            _order = if i == 1
+                filter(x -> x != :l, order)
+            elseif i == n
+                filter(x -> x != :r, order)
             else
-                throw(ArgumentError("Invalid direction: $dir"))
+                order
             end
-        end
-        Tensor(array, inds)
-    end
+
+            inds = map(_order) do dir
+                if dir == :o
+                    symbols[i]
+                elseif dir == :r
+                    symbols[n + mod1(i, n)]
+                elseif dir == :l
+                    symbols[n + mod1(i - 1, n)]
+                else
+                    throw(ArgumentError("Invalid direction: $dir"))
+                end
+            end
+            Tensor(array, inds)
+        end,
+    )
 
     sitemap = Dict(Site(i) => symbols[i] for i in 1:n)
+    qtn = Quantum(tn, sitemap)
 
-    return MPS(Quantum(TensorNetwork(_tensors), sitemap))
+    return MPS(qtn, NonCanonical())
 end
 
-boundary(::MPS) = Open()
+Base.adjoint(tn::MPS) = MPS(adjoint(Quantum(tn)), form(tn))
+
+# TODO different input/output physical dims
+# TODO let choose the orthogonality center
+function Base.rand(rng::Random.AbstractRNG, ::Type{MPS}, n, χ; eltype=Float64, physical_dim=2)
+    p = physical_dim
+    T = eltype
+
+    arrays::Vector{AbstractArray{T,N} where {N}} = map(1:n) do i
+        χl, χr = let after_mid = i > n ÷ 2, i = (n + 1 - abs(2i - n - 1)) ÷ 2
+            χl = min(χ, p^(i - 1))
+            χr = min(χ, p^i)
+
+            # swap bond dims after mid and handle midpoint for odd-length MPS
+            (isodd(n) && i == n ÷ 2 + 1) ? (χl, χl) : (after_mid ? (χr, χl) : (χl, χr))
+        end
+
+        # orthogonalize by QR factorization
+        F = lq!(rand(rng, T, χl, p * χr))
+
+        reshape(Matrix(F.Q), χl, p, χr)
+    end
+
+    # reshape boundary sites
+    arrays[1] = reshape(arrays[1], p, p)
+    arrays[n] = reshape(arrays[n], p, p)
+
+    return MPS(arrays; order=(:l, :o, :r))
+end
