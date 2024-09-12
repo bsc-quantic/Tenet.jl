0382f5764c0982c7d8f3b2ea46fdaf0654db68da
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-09-12 06:00:48 -0400

    Implement `rand`, `adjoint`, `defaultorder`, `boundary`, `form` for `MPS`, `MPO`

--- src/Ansatz/Chain.jl
+++ src/Ansatz/Chain.jl
@@ -11,26 +11,6 @@ Base.copy(tn::Chain) = Chain(copy(Quantum(tn)), boundary(tn))
 Base.similar(tn::Chain) = Chain(similar(Quantum(tn)), boundary(tn))
 Base.zero(tn::Chain) = Chain(zero(Quantum(tn)), boundary(tn))
 
-boundary(tn::Chain) = tn.boundary
-
-MPS(arrays) = Chain(State(), Open(), arrays)
-pMPS(arrays) = Chain(State(), Periodic(), arrays)
-MPO(arrays) = Chain(Operator(), Open(), arrays)
-pMPO(arrays) = Chain(Operator(), Periodic(), arrays)
-
-alias(tn::Chain) = alias(socket(tn), boundary(tn), tn)
-alias(::State, ::Open, ::Chain) = "MPS"
-alias(::State, ::Periodic, ::Chain) = "pMPS"
-alias(::Operator, ::Open, ::Chain) = "MPO"
-alias(::Operator, ::Periodic, ::Chain) = "pMPO"
-
-function Chain(tn::TensorNetwork, sites, args...; kwargs...)
-    return Chain(Quantum(tn, sites), args...; kwargs...)
-end
-
-defaultorder(::Type{Chain}, ::State) = (:o, :l, :r)
-defaultorder(::Type{Chain}, ::Operator) = (:o, :i, :l, :r)
-
 function Chain(::State, boundary::Periodic, arrays::Vector{<:AbstractArray}; order=defaultorder(Chain, State()))
     @assert all(==(3) ∘ ndims, arrays) "All arrays must have 3 dimensions"
     issetequal(order, defaultorder(Chain, State())) ||
@@ -60,45 +40,6 @@ function Chain(::State, boundary::Periodic, arrays::Vector{<:AbstractArray}; ord
     return Chain(Quantum(TensorNetwork(_tensors), sitemap), boundary)
 end
 
-function Chain(::State, boundary::Open, arrays::Vector{<:AbstractArray}; order=defaultorder(Chain, State()))
-    @assert ndims(arrays[1]) == 2 "First array must have 2 dimensions"
-    @assert all(==(3) ∘ ndims, arrays[2:(end - 1)]) "All arrays must have 3 dimensions"
-    @assert ndims(arrays[end]) == 2 "Last array must have 2 dimensions"
-    issetequal(order, defaultorder(Chain, State())) ||
-        throw(ArgumentError("order must be a permutation of $(String.(defaultorder(Chain, State())))"))
-
-    n = length(arrays)
-    gen = IndexCounter()
-    symbols = [nextindex!(gen) for _ in 1:(2n)]
-
-    _tensors = map(enumerate(arrays)) do (i, array)
-        _order = if i == 1
-            filter(x -> x != :l, order)
-        elseif i == n
-            filter(x -> x != :r, order)
-        else
-            order
-        end
-
-        inds = map(_order) do dir
-            if dir == :o
-                symbols[i]
-            elseif dir == :r
-                symbols[n + mod1(i, n)]
-            elseif dir == :l
-                symbols[n + mod1(i - 1, n)]
-            else
-                throw(ArgumentError("Invalid direction: $dir"))
-            end
-        end
-        Tensor(array, inds)
-    end
-
-    sitemap = Dict(Site(i) => symbols[i] for i in 1:n)
-
-    return Chain(Quantum(TensorNetwork(_tensors), sitemap), boundary)
-end
-
 function Chain(::Operator, boundary::Periodic, arrays::Vector{<:AbstractArray}; order=defaultorder(Chain, Operator()))
     @assert all(==(4) ∘ ndims, arrays) "All arrays must have 4 dimensions"
     issetequal(order, defaultorder(Chain, Operator())) ||
@@ -110,48 +51,6 @@ function Chain(::Operator, boundary::Periodic, arrays::Vector{<:AbstractArray}; 
 
     _tensors = map(enumerate(arrays)) do (i, array)
         inds = map(order) do dir
-            if dir == :o
-                symbols[i]
-            elseif dir == :i
-                symbols[i + n]
-            elseif dir == :l
-                symbols[2n + mod1(i - 1, n)]
-            elseif dir == :r
-                symbols[2n + mod1(i, n)]
-            else
-                throw(ArgumentError("Invalid direction: $dir"))
-            end
-        end
-        Tensor(array, inds)
-    end
-
-    sitemap = Dict(Site(i) => symbols[i] for i in 1:n)
-    merge!(sitemap, Dict(Site(i; dual=true) => symbols[i + n] for i in 1:n))
-
-    return Chain(Quantum(TensorNetwork(_tensors), sitemap), boundary)
-end
-
-function Chain(::Operator, boundary::Open, arrays::Vector{<:AbstractArray}; order=defaultorder(Chain, Operator()))
-    @assert ndims(arrays[1]) == 3 "First array must have 3 dimensions"
-    @assert all(==(4) ∘ ndims, arrays[2:(end - 1)]) "All arrays must have 4 dimensions"
-    @assert ndims(arrays[end]) == 3 "Last array must have 3 dimensions"
-    issetequal(order, defaultorder(Chain, Operator())) ||
-        throw(ArgumentError("order must be a permutation of $(String.(defaultorder(Chain, Operator())))"))
-
-    n = length(arrays)
-    gen = IndexCounter()
-    symbols = [nextindex!(gen) for _ in 1:(3n - 1)]
-
-    _tensors = map(enumerate(arrays)) do (i, array)
-        _order = if i == 1
-            filter(x -> x != :l, order)
-        elseif i == n
-            filter(x -> x != :r, order)
-        else
-            order
-        end
-
-        inds = map(_order) do dir
             if dir == :o
                 symbols[i]
             elseif dir == :i
@@ -205,82 +104,6 @@ function rightindex(::Open, tn::Chain, site::Site)
     return site == Site(nlanes(tn); dual=isdual(site)) ? nothing : rightindex(Periodic(), tn, site)
 end
 rightindex(::Periodic, tn::Chain, site::Site) = inds(tn; bond=(site, rightsite(tn, site)))
-
-Base.adjoint(chain::Chain) = Chain(adjoint(Quantum(chain)), boundary(chain))
-
-struct ChainSampler{B<:Boundary,S<:Socket,NT<:NamedTuple} <: Random.Sampler{Chain}
-    parameters::NT
-
-    ChainSampler{B,S}(; kwargs...) where {B,S} = new{B,S,typeof(values(kwargs))}(values(kwargs))
-end
-
-function Base.rand(A::Type{<:Chain}, B::Type{<:Boundary}, S::Type{<:Socket}; kwargs...)
-    return rand(Random.default_rng(), A, B, S; kwargs...)
-end
-
-function Base.rand(rng::AbstractRNG, ::Type{A}, ::Type{B}, ::Type{S}; kwargs...) where {A<:Chain,B<:Boundary,S<:Socket}
-    return rand(rng, ChainSampler{B,S}(; kwargs...), B, S)
-end
-
-# TODO let choose the orthogonality center
-function Base.rand(rng::Random.AbstractRNG, sampler::ChainSampler, ::Type{Open}, ::Type{State})
-    n = sampler.parameters.n
-    χ = sampler.parameters.χ
-    p = get(sampler.parameters, :p, 2)
-    T = get(sampler.parameters, :eltype, Float64)
-
-    arrays::Vector{AbstractArray{T,N} where {N}} = map(1:n) do i
-        χl, χr = let after_mid = i > n ÷ 2, i = (n + 1 - abs(2i - n - 1)) ÷ 2
-            χl = min(χ, p^(i - 1))
-            χr = min(χ, p^i)
-
-            # swap bond dims after mid and handle midpoint for odd-length MPS
-            (isodd(n) && i == n ÷ 2 + 1) ? (χl, χl) : (after_mid ? (χr, χl) : (χl, χr))
-        end
-
-        # orthogonalize by QR factorization
-        F = lq!(rand(rng, T, χl, p * χr))
-
-        reshape(Matrix(F.Q), χl, p, χr)
-    end
-
-    # reshape boundary sites
-    arrays[1] = reshape(arrays[1], p, p)
-    arrays[n] = reshape(arrays[n], p, p)
-
-    return Chain(State(), Open(), arrays; order=(:l, :o, :r))
-end
-
-# TODO different input/output physical dims
-function Base.rand(rng::Random.AbstractRNG, sampler::ChainSampler, ::Type{Open}, ::Type{Operator})
-    n = sampler.parameters.n
-    χ = sampler.parameters.χ
-    p = get(sampler.parameters, :p, 2)
-    T = get(sampler.parameters, :eltype, Float64)
-
-    ip = op = p
-
-    arrays::Vector{AbstractArray{T,N} where {N}} = map(1:n) do i
-        χl, χr = let after_mid = i > n ÷ 2, i = (n + 1 - abs(2i - n - 1)) ÷ 2
-            χl = min(χ, ip^(i - 1) * op^(i - 1))
-            χr = min(χ, ip^i * op^i)
-
-            # swap bond dims after mid and handle midpoint for odd-length MPS
-            (isodd(n) && i == n ÷ 2 + 1) ? (χl, χl) : (after_mid ? (χr, χl) : (χl, χr))
-        end
-
-        # orthogonalize by QR factorization
-        F = lq!(rand(rng, T, χl, ip * op * χr))
-        reshape(Matrix(F.Q), χl, ip, op, χr)
-    end
-
-    # reshape boundary sites
-    arrays[1] = reshape(arrays[1], p, p, min(χ, ip * op))
-    arrays[n] = reshape(arrays[n], min(χ, ip * op), p, p)
-
-    # TODO order might not be the best for performance
-    return Chain(Operator(), Open(), arrays; order=(:l, :i, :o, :r))
-end
 
 # """
 #     Tenet.contract!(tn::Chain; between=(site1, site2), direction::Symbol = :left, delete_Λ = true)
--- src/Ansatz/MPO.jl
+++ src/Ansatz/MPO.jl
@@ -1,47 +1,85 @@
+using Random
+
 struct MPO <: Ansatz
     super::Quantum
+    form::Form
 end
+
+defaultorder(::Type{MPO}) = (:o, :i, :l, :r)
+boundary(::MPO) = Open()
+form(tn::MPO) = tn.form
 
 function MPO(arrays::Vector{<:AbstractArray}; order=defaultorder(MPO))
     @assert ndims(arrays[1]) == 3 "First array must have 3 dimensions"
     @assert all(==(4) ∘ ndims, arrays[2:(end - 1)]) "All arrays must have 4 dimensions"
     @assert ndims(arrays[end]) == 3 "Last array must have 3 dimensions"
-    issetequal(order, defaultorder(Chain, Operator())) ||
-        throw(ArgumentError("order must be a permutation of $(String.(defaultorder(Chain, Operator())))"))
+    issetequal(order, defaultorder(MPO)) ||
+        throw(ArgumentError("order must be a permutation of $(String.(defaultorder(MPO)))"))
 
     n = length(arrays)
     gen = IndexCounter()
     symbols = [nextindex!(gen) for _ in 1:(3n - 1)]
 
-    _tensors = map(enumerate(arrays)) do (i, array)
-        _order = if i == 1
-            filter(x -> x != :l, order)
-        elseif i == n
-            filter(x -> x != :r, order)
-        else
-            order
-        end
-
-        inds = map(_order) do dir
-            if dir == :o
-                symbols[i]
-            elseif dir == :i
-                symbols[i + n]
-            elseif dir == :l
-                symbols[2n + mod1(i - 1, n)]
-            elseif dir == :r
-                symbols[2n + mod1(i, n)]
+    tn = TensorNetwork(
+        map(enumerate(arrays)) do (i, array)
+            _order = if i == 1
+                filter(x -> x != :l, order)
+            elseif i == n
+                filter(x -> x != :r, order)
             else
-                throw(ArgumentError("Invalid direction: $dir"))
+                order
             end
-        end
-        Tensor(array, inds)
-    end
+
+            inds = map(_order) do dir
+                if dir == :o
+                    symbols[i]
+                elseif dir == :i
+                    symbols[i + n]
+                elseif dir == :l
+                    symbols[2n + mod1(i - 1, n)]
+                elseif dir == :r
+                    symbols[2n + mod1(i, n)]
+                else
+                    throw(ArgumentError("Invalid direction: $dir"))
+                end
+            end
+            Tensor(array, inds)
+        end,
+    )
 
     sitemap = Dict(Site(i) => symbols[i] for i in 1:n)
     merge!(sitemap, Dict(Site(i; dual=true) => symbols[i + n] for i in 1:n))
+    qtn = Quantum(tn, sitemap)
 
-    return MPO(Quantum(TensorNetwork(_tensors), sitemap))
+    return MPO(qtn, NonCanonical())
 end
 
-boundary(::MPO) = Open()
+Base.adjoint(tn::MPO) = MPO(adjoint(Quantum(tn)), form(tn))
+
+# TODO different input/output physical dims
+# TODO let choose the orthogonality center
+function Base.rand(rng::Random.AbstractRNG, ::Type{MPO}, n, χ; eltype=Float64, physical_dim=2)
+    T = eltype
+    ip = op = physical_dim
+
+    arrays::Vector{AbstractArray{T,N} where {N}} = map(1:n) do i
+        χl, χr = let after_mid = i > n ÷ 2, i = (n + 1 - abs(2i - n - 1)) ÷ 2
+            χl = min(χ, ip^(i - 1) * op^(i - 1))
+            χr = min(χ, ip^i * op^i)
+
+            # swap bond dims after mid and handle midpoint for odd-length MPS
+            (isodd(n) && i == n ÷ 2 + 1) ? (χl, χl) : (after_mid ? (χr, χl) : (χl, χr))
+        end
+
+        # orthogonalize by QR factorization
+        F = lq!(rand(rng, T, χl, ip * op * χr))
+        reshape(Matrix(F.Q), χl, ip, op, χr)
+    end
+
+    # reshape boundary sites
+    arrays[1] = reshape(arrays[1], p, p, min(χ, ip * op))
+    arrays[n] = reshape(arrays[n], min(χ, ip * op), p, p)
+
+    # TODO order might not be the best for performance
+    return MPO(arrays; order=(:l, :i, :o, :r))
+end
