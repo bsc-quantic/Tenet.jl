edced2deb2547d880d834f1aff0e5c3f9d9fde28
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-09-16 00:47:18 -0400

    Delete legacy `RingPeek`

--- test/Helpers_test.jl
+++ test/Helpers_test.jl
@@ -1,12 +1,4 @@
 @testset "Helpers" begin
-    @testset "RingPeek" begin
-        using Tenet: ringpeek
-
-        it = ringpeek([0 1 2])
-        @test length(it) == 3
-        @test collect(it) == [(0, 1), (1, 2), (2, 0)]
-    end
-
     @testset "letter" begin
         using Tenet: letter
         # NOTE probabilitic testing due to time taken by `letter`. refactor when `letter` is optimized.
