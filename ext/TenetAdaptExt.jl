c4678ba74b8558c7a8d319cfe2ef853a48d73323
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-09-18 14:43:19 -0400

    Refactor `adapt_structure` method to support additional types

--- ext/TenetAdaptExt.jl
+++ ext/TenetAdaptExt.jl
@@ -7,7 +7,6 @@ Adapt.adapt_structure(to, x::Tensor) = Tensor(adapt(to, parent(x)), inds(x))
 Adapt.adapt_structure(to, x::TensorNetwork) = TensorNetwork(adapt.(Ref(to), tensors(x)))
 
 Adapt.adapt_structure(to, x::Quantum) = Quantum(adapt(to, TensorNetwork(x)), x.sites)
-Adapt.adapt_structure(to, x::Product) = Product(adapt(to, Quantum(x)))
-Adapt.adapt_structure(to, x::Chain) = Chain(adapt(to, Quantum(x)), boundary(x))
+Adapt.adapt_structure(to, x::Ansatz) = Ansatz(adapt(to, Quantum(x)), lattice(x))
 
 end
