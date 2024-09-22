43eba7d5005cbf30598d69409ef505c9d1abff2c
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-09-22 02:32:20 -0400

    Convert from Quimb's TensorNetwork to `TensorNetwork`

---
+++ ext/TenetPythonCallExt/Quimb.jl
@@ -0,0 +1,14 @@
+function Tenet.TensorNetwork(::Val{:quimb}, pyobj::Py)
+    quimb = pyimport("quimb")
+    if pyissubclass(pytype(pyobj), quimb.tensor.tensor_core.TensorNetwork)
+        throw(ArgumentError("Expected a quimb's TensorNetwork object, got $(pyfullyqualname(pyobj))"))
+    end
+
+    ts = map(pyobj.tensors) do tensor
+        array = pyconvert(Array, tensor.data)
+        inds = Symbol.(pyconvert(Array, tensor.inds))
+        Tensor(array, inds)
+    end
+
+    return TensorNetwork(ts)
+end
--- ext/TenetPythonCallExt/TenetPythonCallExt.jl
+++ ext/TenetPythonCallExt/TenetPythonCallExt.jl
@@ -6,11 +6,17 @@ using PythonCall.Core: pyisnone
 
 pyfullyqualname(pyobj) = join([pytype(pyobj).__module__, pytype(pyobj).__qualname__], '.')
 
+function Tenet.TensorNetwork(pyobj::Py)
+    pymodule, _ = split(pytype(pyobj).__module__, "."; limit=1)
+    return TensorNetwork(Val(Symbol(pymodule)), pyobj)
+end
+
 function Tenet.Quantum(pyobj::Py)
     pymodule, _ = split(pytype(pyobj).__module__, "."; limit=1)
     return Quantum(Val(Symbol(pymodule)), pyobj)
 end
 
 include("Qiskit.jl")
+include("Quimb.jl")
 
 end
