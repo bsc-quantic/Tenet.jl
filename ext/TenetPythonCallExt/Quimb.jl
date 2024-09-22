9926fd598d3ead095be48507b0011a2584bb96e1
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-09-22 16:36:06 -0400

    Implement Quimb's TensorNetworkGenVector, TensorNetworkGenOperator conversion to `Quantum`

--- ext/TenetPythonCallExt/Quimb.jl
+++ ext/TenetPythonCallExt/Quimb.jl
@@ -12,3 +12,33 @@ function Tenet.TensorNetwork(::Val{:quimb}, pyobj::Py)
 
     return TensorNetwork(ts)
 end
+
+function Tenet.Quantum(::Val{:quimb}, pyobj::Py)
+    quimb = pyimport("quimb")
+    if pyissubclass(pytype(pyobj), quimb.tensor.circuit.Circuit)
+        return Quantum(pyobj.get_uni())
+    elseif pyissubclass(pytype(pyobj), quimb.tensor.tensor_arbgeom.TensorNetworkGenVector)
+        return Quantum(Val(Symbol("quimb.tensor.tensor_arbgeom.TensorNetworkGenVector")), pyobj)
+    elseif pyissubclass(pytype(pyobj), quimb.tensor.tensor_arbgeom.TensorNetworkGenOperator)
+        return Quantum(Val(Symbol("quimb.tensor.tensor_arbgeom.TensorNetworkGenOperator")), pyobj)
+    else
+        throw(ArgumentError("Unknown treatment for object of class $(pyfullyqualname(pyobj))"))
+    end
+end
+
+function Tenet.Quantum(::Val{Symbol("quimb.tensor.tensor_arbgeom.TensorNetworkGenVector")}, pyobj::Py)
+    tn = TensorNetwork(pyobj)
+    sitedict = Dict(Site(i) => Symbol(pyobj.lower_ind(i)) for i in pyobj.sites)
+    return Quantum(tn, sitedict)
+end
+
+function Tenet.Quantum(::Val{Symbol("quimb.tensor.tensor_arbgeom.TensorNetworkGenOperator")}, pyobj::Py)
+    tn = TensorNetwork(pyobj)
+
+    sitedict = merge!(
+        Dict(Site(i) => Symbol(pyobj.lower_ind(i)) for i in pyobj.sites),
+        Dict(Site(i; dual=true) => Symbol(pyobj.upper_ind(i)) for i in pyobj.sites),
+    )
+
+    return Quantum(tn, sitedict)
+end
