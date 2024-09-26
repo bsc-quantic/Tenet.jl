ff5002eb64399dc787cd0b9370ea82a5d9cb1cce
Jofre <jofrevalles99@gmail.com>
2024-09-26 14:58:57 +0200

    Enhance Yao extension tests

--- test/integration/YaoBlocks_test.jl
+++ test/integration/YaoBlocks_test.jl
@@ -7,4 +7,24 @@
 
     @test issetequal(sites(tn), [site"1", site"2", site"1'", site"2'"])
     @test Tenet.ntensors(tn) == 2
+
+    @testset "GHZ Circuit" begin
+        circuit_GHZ = chain(
+            n_qubits,
+            put(1=>Yao.H),
+            Yao.control(1, 2=>Yao.X),
+            Yao.control(2, 3=>Yao.X)
+        )
+
+        quantum_circuit = Quantum(circuit_GHZ)
+
+        zeros = Quantum(Product(fill([1, 0], n_qubits))) #|000>
+        ones = Quantum(Product(fill([0, 1], n_qubits))) #|111>
+
+        expected_value = Tenet.contract(merge(zeros, quantum_circuit, ones')) # <111|circuit|000>
+        @test only(expected_value) ≈ 1/√2
+
+        SV_Yao = apply!(zero_state(n_qubits), circuit_GHZ) # circuit|000>
+        @test only(statevec(ArrayReg(bit"111"))' * statevec(SV_Yao)) ≈ 1/√2
+    end
 end
