5b8da11e659dfd63a97c5c0f059989d1b48765bd
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-09-26 17:58:15 -0400

    Fix reference to lattice in `adapt_structure` for `Ansatz`

--- ext/TenetAdaptExt.jl
+++ ext/TenetAdaptExt.jl
@@ -7,7 +7,7 @@ Adapt.adapt_structure(to, x::Tensor) = Tensor(adapt(to, parent(x)), inds(x))
 Adapt.adapt_structure(to, x::TensorNetwork) = TensorNetwork(adapt.(Ref(to), tensors(x)))
 
 Adapt.adapt_structure(to, x::Quantum) = Quantum(adapt(to, TensorNetwork(x)), x.sites)
-Adapt.adapt_structure(to, x::Ansatz) = Ansatz(adapt(to, Quantum(x)), lattice(x))
+Adapt.adapt_structure(to, x::Ansatz) = Ansatz(adapt(to, Quantum(x)), Tenet.lattice(x))
 
 Adapt.adapt_structure(to, x::Product) = Product(adapt(to, Ansatz(x)))
 Adapt.adapt_structure(to, x::Dense) = Dense(adapt(to, Ansatz(x)))
