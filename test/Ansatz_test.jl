a80ef9cb6270ca3995ed9757808119c090f54c19
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-11-02 20:23:48 +0100

    test `Ansatz`

--- test/Ansatz_test.jl
+++ test/Ansatz_test.jl
@@ -1,2 +1,52 @@
+using Tenet
+using Tenet: Lattice
+using BijectiveDicts: BijectiveIdDict
+using Graphs
+
 @testset "Ansatz" begin
+    @testset "No connectivity" let tn = TensorNetwork([Tensor(zeros(2), [:i]), Tensor(zeros(2), [:j])]),
+        qtn = Quantum(tn, Dict(site"1" => :i, site"2" => :j)),
+        graph = Graph(2),
+        mapping = BijectiveIdDict{Site,Int}(Pair{Site,Int}[Site(i) => i for i in 1:2]),
+        lattice = Lattice(mapping, graph),
+        ansatz = Ansatz(qtn, lattice)
+
+        @test zero(ansatz) == Ansatz(zero(qtn), lattice)
+        @test lattice(ansatz) == lattice
+        @test isempty(neighbors(ansatz; at=site"1"))
+        @test !Tenet.has_edge(ansatz, site"1", site"2")
+
+        # the following methods will throw a AssertionError in here, but it's not a hard API requirement
+        @test_throws Exception inds(ansatz; bond=(site"1", site"2"))
+        @test_throws Exception tensors(ansatz; bond=(site"1", site"2"))
+        @test_throws Exception truncate!(ansatz, bond=(site"1", site"2"))
+
+        # TODO test `expect`, `overlap`, `evolve!`, `simple_update!`
+    end
+
+    @testset_skip "Complete connectivity" let n = 2,
+        graph = Graphs.complete_graph(2),
+        mapping = BijectiveIdDict{Site,Int}(Pair{Site,Int}[Site(i) => i for i in 1:n]),
+        lattice = Lattice(mapping, graph),
+        tn = TensorNetwork([Tensor(ones(2, 2, 2), [:s1, :i]), Tensor(ones(2, 2, 2), [:s2, :i])]),
+        qtn = Quantum(tn, Dict(site"1" => :s1, site"2" => :s2)),
+        ansatz = Ansatz(qtn, lattice)
+
+        @test zero(ansatz) == Ansatz(zero(qtn), lattice)
+        @test lattice(ansatz) == lattice
+
+        @test issetequal(neighbors(ansatz; at=site"1"), [site"2"])
+        @test issetequal(neighbors(ansatz; at=site"2"), [site"1"])
+
+        @test Tenet.has_edge(ansatz, site"1", site"2")
+        @test Tenet.has_edge(ansatz, site"2", site"1")
+
+        @test inds(ansatz; bond=(site"1", site"2")) == :j
+
+        # the following methods will throw a AssertionError in here, but it's not a hard API requirement
+        @test_throws Exception tensors(ansatz; bond=(site"1", site"2"))
+        @test_throws Exception truncate!(ansatz, bond=(site"1", site"2"))
+
+        # TODO test `expect`, `overlap`, `evolve!`, `simple_update!`
+    end
 end
