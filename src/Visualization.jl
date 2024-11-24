887aa8fab3036df70faf324918dfada488e92187
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-11-24 14:12:25 +0100

    refactor vega+json spec generation

--- src/Visualization.jl
+++ src/Visualization.jl
@@ -1,4 +1,5 @@
 using Graphs: Graphs, vertices, edges
+using OrderedCollections: OrderedDict
 
 function graph_representation(tn::AbstractTensorNetwork)
     tn = TensorNetwork(tn)
@@ -38,107 +39,144 @@ function Base.Multimedia.istextmime(::MIME"application/vnd.vega.v5+json")
 end
 
 Base.show(io::IO, ::MIME"application/vnd.vega.v5+json", @nospecialize(tn::AbstractTensorNetwork)) = draw(io, tn)
-function draw(io::IO, @nospecialize(tn::AbstractTensorNetwork))
+
+@inline draw(io::IO, args...) = draw(IOContext(io), args...)
+
+function draw(io::IOContext, @nospecialize(tn::AbstractTensorNetwork))
+    width = get(io, :width, 800)
+    height = get(io, :height, 600)
+
     tn, graph, tensormap, hypermap, hypernodes, ghostnodes = graph_representation(tn)
     hypermap = Dict(Iterators.flatten([[i => v for i in k] for (k, v) in hypermap]))
 
-    json = """{
-            "\$schema": "https://vega.github.io/schema/vega/v5.json",
-            "width": 600,
-            "height": 600,
-            "signals": [
-                {"name": "center.x", "update": "width / 2"},
-                {"name": "center.y", "update": "height / 2"},
+    spec = OrderedDict{Symbol,Any}()
+    spec[Symbol("\$schema")] = "https://vega.github.io/schema/vega/v5.json"
+    spec[:width] = width
+    spec[:height] = height
+    spec[:signals] = [
+        OrderedDict(:name => "center.x", :update => "width / 2", :__collapse => true),
+        OrderedDict(:name => "center.y", :update => "height / 2", :__collapse => true),
+    ]
+    vdata = OrderedDict{Symbol,Any}()
+    edata = OrderedDict{Symbol,Any}()
+    spec[:data] = [vdata, edata]
+    vmarks = OrderedDict{Symbol,Any}()
+    emarks = OrderedDict{Symbol,Any}()
+    spec[:marks] = [vmarks, emarks]
+
+    # vertex data
+    vdata[:name] = "vertex-data"
+    vdata[:format] = OrderedDict(:type => "json", :__collapse => true)
+    vdata[:values] = map(vertices(graph)) do v
+        group = if v ∈ ghostnodes
+            0
+        elseif v ∈ hypernodes
+            1
+        else
+            2
+        end
+        OrderedDict(:name => v, :group => group, :__collapse => true)
+    end
+
+    # edge data
+    edata[:name] = "edge-data"
+    edata[:format] = OrderedDict(:type => "json", :__collapse => true)
+    edata[:values] = map(inds(tn)) do i
+        nodes = tensors(tn; intersects=i)
+
+        if length(nodes) == 2
+            # regular nodes
+            a, b = nodes
+            index = get(hypermap, i, i)
+            OrderedDict(
+                :source => tensormap[a] - 1,
+                :target => tensormap[b] - 1,
+                :value => string(index),
+                :__collapse => true,
+            )
+
+        elseif length(nodes) == 1
+            # nodes with open indices
+            vertex = tensormap[only(nodes)] - 1
+            ghost = ntensors(tn) + vertex
+            OrderedDict(:source => vertex, :target => ghost, :value => string(i), :__collapse => true)
+        end
+    end
+
+    # vertex marks
+    vmarks[:name] = "vertices"
+    vmarks[:type] = "symbol"
+    vmarks[:from] = OrderedDict(:data => "vertex-data", :__collapse => true)
+    vmarks[:transform] = [
+        OrderedDict(
+            :type => "force",
+            :static => false,
+            :iterations => 1000,
+            :forces => [
+                OrderedDict(
+                    :force => "center",
+                    :x => OrderedDict(:signal => "center.x", :__collapse => true),
+                    :y => OrderedDict(:signal => "center.y", :__collapse => true),
+                    :__collapse => true,
+                ),
+                OrderedDict(:force => "collide", :radius => 10, :__collapse => true),
+                OrderedDict(:force => "nbody", :strength => -100, :__collapse => true),
+                OrderedDict(:force => "link", :links => "edge-data", :distance => 100, :__collapse => true),
             ],
-            "data": [
-                {
-                    "name": "vertex-data",
-                    "format": {"type": "json" },
-                    "values": [\n\t\t$(
-                        join(map(vertices(graph)) do v
-                            return "{ \"name\": \"$v\", \"group\": $(
-                            if v ∈ ghostnodes
-                                0 # "\"ghost\""
-                            elseif v ∈ hypernodes
-                                1 # "\"hyper\""
-                            else
-                                2 # "\"tensor\""
-                            end) }"
-                        end,
-                        ",\n\t\t"))
-                    ]
-                },
-                {
-                    "name": "edge-data",
-                    "format": {"type": "json" },
-                    "values": [\n\t\t$(
-                        join(map(inds(tn)) do i
-                            # NOTE Vega uses 0-indexing
-                            nodes = tensors(tn; intersects=i)
+        ),
+    ]
 
-                            # regular nodes
-                            if length(nodes) == 2
-                                a, b = nodes
-                                index = get(hypermap, i, i)
-                                return "{ \"source\": $(tensormap[a] - 1), \"target\": $(tensormap[b] - 1), \"value\": \"$index\" }"
+    # edge marks
+    emarks[:type] = "path"
+    emarks[:from] = OrderedDict(:data => "edge-data", :__collapse => true)
+    emarks[:transform] = [
+        OrderedDict(
+            :type => "linkpath",
+            :shape => "line",
+            :sourceX => "datum.source.x",
+            :sourceY => "datum.source.y",
+            :targetX => "datum.target.x",
+            :targetY => "datum.target.y",
+        ),
+    ]
 
-                            # nodes with open indices
-                            elseif length(nodes) == 1
-                                vertex = tensormap[only(nodes)] - 1
-                                # TODO is this correct? what if more than 1 open index (e.g. MPO)
-                                ghost = ntensors(tn) + vertex
-                                return "{ \"source\": $vertex, \"target\": $ghost, \"value\": \"$i\" }"
-                            end
-                        end,
-                        ",\n\t\t")
-                        )
-                    ]
-                }
-            ],
-            "marks": [
-                {
-                    "name": "vertices",
-                    "type": "symbol",
-                    "from": {"data": "vertex-data"},
-                    "encode": {
-                    },
-                    "transform": [
-                        {
-                            "type": "force",
-                            "static": false,
-                            "iterations": 1000,
-                            "forces": [
-                                {"force": "center", "x": {"signal": "center.x"}, "y": {"signal": "center.y"}},
-                                {"force": "collide", "radius": 10},
-                                {"force": "nbody", "strength": -100},
-                                {"force": "link", "links": "edge-data", "distance": 100},
-                            ]
-                        }
-                    ]
-                },
-                {
-                    "type": "path",
-                    "from": {"data": "edge-data"},
-                    "encode": {
-                        "update": {
-                            "stroke": {"value": "#ccc"},
-                            "strokeWidth": {"value": 2},
-                            "tooltip": {"field": "name"}
-                        }
-                    },
-                    "transform": [
-                        {
-                            "type": "linkpath",
-                            "shape": "line",
-                            "sourceX": "datum.source.x",
-                            "sourceY": "datum.source.y",
-                            "targetX": "datum.target.x",
-                            "targetY": "datum.target.y"
-                        }
-                    ]
-                }
-            ]
-        }"""
+    return print_json(io, spec)
+end
 
-    return print(io, json)
+function print_json(io::IO, spec; indent::Int=0)
+    spec = copy(spec)
+    collapse = get(spec, :__collapse, false)
+    delete!(spec, :__collapse)
+
+    print(io, '\t'^indent * "{" * (collapse ? "" : "\n"))
+
+    for (i, (key, value)) in enumerate(spec)
+        print(io, (collapse ? "" : '\t'^(indent + 1)) * "\"$key\": ")
+
+        if isa(value, AbstractDict)
+            print_json(io, value; indent=0)
+
+        elseif isa(value, Vector)
+            print(io, "[")
+            for (j, v) in enumerate(value)
+                print(io, collapse ? "" : "\n")
+                print_json(io, v; indent=indent + 2)
+
+                islast = j == length(value)
+                print(io, (islast ? "" : ","))
+            end
+            print(io, "\n" * '\t'^(indent + 1) * "]")
+
+        elseif isa(value, AbstractString)
+            print(io, "\"$value\"")
+
+        else
+            print(io, value)
+        end
+
+        islast = i == length(spec)
+        print(io, (islast ? "" : ",") * (collapse ? "" : "\n"))
+    end
+
+    print(io, (collapse ? "" : '\t'^indent) * "}")
 end
