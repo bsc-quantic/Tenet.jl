dd14d20592dfbb7c4dd036f37389d1a4f5ca0a05
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-12-07 16:39:12 +0100

    fix tests

--- test/integration/Reactant_test.jl
+++ test/integration/Reactant_test.jl
@@ -16,15 +16,23 @@
             Are = adapt(ConcreteRArray, A)
             Bre = adapt(ConcreteRArray, B)
             Cre = @jit contract(Are, Bre)
+
             @test inds(Cre) == inds(C)
             @test size(Cre) == size(C)
             @test parent(Cre) ≈ parent(C)
 
+            A = Tensor(rand(2, 3), (:i, :j))
+            B = Tensor(rand(4, 3), (:j, :k))
+            Are = adapt(ConcreteRArray, A)
+            Bre = adapt(ConcreteRArray, B)
+
             f(a, b) = contract(a, b; out=[:k, :i])
+            C = f(A, B)
             Cre = @jit f(Are, Bre)
-            @test inds(C) == reverse(inds(Cre))
-            @test size(C) == reverse(size(Cre))
-            @test parent(C) ≈ transpose(parent(Cre))
+
+            @test inds(Cre) == inds(C)
+            @test size(Cre) == size(C)
+            @test parent(Cre) ≈ parent(C)
         end
     end
 end
