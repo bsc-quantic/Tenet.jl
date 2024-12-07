f5acb670c666488c56622ccf8d5a45c4b07d8e06
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-12-07 16:41:54 +0100

    refactor conversion to `TracedRArray`

--- ext/TenetReactantExt.jl
+++ ext/TenetReactantExt.jl
@@ -207,7 +207,7 @@ end
 
 Tenet.contract(a::Tensor, b::Tensor{T,N,TracedRArray{T,N}}; kwargs...) where {T,N} = contract(b, a; kwargs...)
 function Tenet.contract(a::Tensor{Ta,Na,TracedRArray{Ta,Na}}, b::Tensor{Tb,Nb}; kwargs...) where {Ta,Na,Tb,Nb}
-    return contract(a, Tensor(TracedRArray{Tb,Nb}, parent(b)); kwargs...)
+    return contract(a, Tensor(Reactant.Ops.constant(parent(b)), inds(b)); kwargs...)
 end
 
 end
