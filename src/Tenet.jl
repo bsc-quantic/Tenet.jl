2f587d3d12c6d913a96c54a16c3b548c1ecb5db8
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-12-11 00:45:08 +0100

    Use `Compat.@compat` to mark public functions not exported

--- src/Tenet.jl
+++ src/Tenet.jl
@@ -1,8 +1,10 @@
 module Tenet
 
 import EinExprs: inds
+using Compat
 
 include("Helpers.jl")
+@compat public letter, nonunique, IndexCounter, currindex, nextindex!
 
 include("Tensor.jl")
 export Tensor, contract, dim, expand
@@ -10,35 +12,62 @@ export Tensor, contract, dim, expand
 include("Numerics.jl")
 
 include("TensorNetwork.jl")
-export TensorNetwork, tensors, arrays, neighbors, slice!
-export contract, contract!, groupinds!
+export TensorNetwork, tensors, arrays, neighbors, slice!, contract, contract!, groupinds!
+@compat public AbstractTensorNetwork, ninds, ntensors, @unsafe_region, tryprune!, resetindex!
 
 include("Transformations.jl")
 export transform, transform!
+#! format: off
+@compat public Transformation,
+    HyperFlatten,
+    HyperGroup,
+    ContractSimplification,
+    Truncate,
+    DiagonalReduction,
+    AntiDiagonalGauging,
+    SplitSimplification
+#! format: on
 
 include("Site.jl")
 export Site, @site_str, isdual
+@compat public id
 
 include("Quantum.jl")
-export Quantum, ninputs, noutputs, inputs, outputs, sites, nsites
+export Quantum, ninputs, noutputs, inputs, outputs, sites, lanes, socket
+@compat public AbstractQuantum, Socket, Scalar, State, Operator, reindex!, @reindex!, nsites, nlanes, hassite
 
 include("Lattice.jl")
+@compat public Lattice
 
 include("Ansatz.jl")
-export Ansatz
-export socket, Scalar, State, Operator
-export boundary, Open, Periodic
-export form, NonCanonical, MixedCanonical, Canonical
-
-export canonize, canonize!, mixed_canonize, mixed_canonize!, truncate!, isisometry
+#! format: off
+export Ansatz,
+    boundary,
+    Open,
+    Periodic,
+    form,
+    NonCanonical,
+    MixedCanonical,
+    Canonical,
+    canonize,
+    canonize!,
+    mixed_canonize,
+    mixed_canonize!,
+    truncate!,
+    isisometry,
+    expect,
+    evolve!,
+    simple_update!,
+    overlap
+#! format: on
+@compat public AbstractAnsatz, Boundary, Form
 
 include("Product.jl")
 export Product
 
 include("MPS.jl")
 export MPS, MPO
-
-export evolve!, expect, overlap
+@compat public AbstractMPS, AbstractMPO, defaultorder, check_form
 
 # reexports from EinExprs
 export einexpr, inds
