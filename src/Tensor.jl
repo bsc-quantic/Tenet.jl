cf893300cfd5c4ceee595d8173675cedd5f28d2b
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-12-26 11:36:34 +0100

    Undo conversion to `Tuple` on `inds(::Tensor)`

--- src/Tensor.jl
+++ src/Tensor.jl
@@ -39,7 +39,7 @@ Tensor(data::Number) = Tensor(fill(data))
 
 Return the indices of the tensor in the order of the dimensions.
 """
-inds(t::Tensor) = Tuple(t.inds)
+inds(t::Tensor) = t.inds
 
 function Base.copy(t::Tensor{T,N,<:SubArray{T,N}}) where {T,N}
     data = copy(t.data)
