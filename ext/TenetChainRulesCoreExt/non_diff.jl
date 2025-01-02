ffa1980022df7a874cee095657f89bd78d512f55
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-01-02 16:11:08 +0100

    Delete legacy `__omeinsum_sym2str`

--- ext/TenetChainRulesCoreExt/non_diff.jl
+++ ext/TenetChainRulesCoreExt/non_diff.jl
@@ -1,6 +1,3 @@
-# NOTE fix problem with vector generator in `contract`
-@non_differentiable Tenet.__omeinsum_sym2str(x)
-
 # WARN type-piracy
 @non_differentiable setdiff(s::Base.AbstractVecOrTuple{Symbol}, itrs::Base.AbstractVecOrTuple{Symbol}...)
 @non_differentiable union(s::Base.AbstractVecOrTuple{Symbol}, itrs::Base.AbstractVecOrTuple{Symbol}...)
