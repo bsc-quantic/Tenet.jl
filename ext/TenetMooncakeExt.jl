2c4c468c15705eb1427d94290ca3ba8b57e06b45
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-01-08 15:51:00 +0100

    Implement `to_cr_tangent` for `Tensor` type

--- ext/TenetMooncakeExt.jl
+++ ext/TenetMooncakeExt.jl
@@ -3,6 +3,8 @@ module TenetMooncakeExt
 using Tenet
 using Mooncake: Mooncake, @from_rrule, DefaultCtx
 
+Mooncake.to_cr_tangent(tensor::Tensor) = tensor
+
 @from_rrule DefaultCtx Tuple{typeof(contract),Tensor} true
 @from_rrule DefaultCtx Tuple{typeof(contract),Tensor,Tensor} true
 
