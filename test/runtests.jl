dda11aff7bca84b807eddf5b76c3aa8911764904
Jofre Vallès Muns <61060572+jofrevalles@users.noreply.github.com>
2025-01-15 15:18:05 +0100

    Parallelize `CI` by splitting Unit Tests and Integration Tests on different Github actions (#288)
    * Update runtests in order to split Integration and Unit tests actions
    
    * Update CI.yml in order to split Unit and Integration Tests actions
    
    * Fix indent in code
    
    * Fix indent in code
    
    * Update runtests by passing the target testset in an ENV
    
    * Enhance CI.yml for changes in runtests.jl
    
    * Fix typo in runtests.jl
    
    * Move Reactant import from runtests.jl to Reactant_tests.jl
    
    * Fix typo on CI.yml
    
    * Add temporal debug print on runtests.jl
    
    * Fix env setting from CI.yml
    
    * Apply @mofeing suggestions from code review
    
    Co-authored-by: Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,45 +1,50 @@
 using Test
 using Tenet
 using OMEinsum
-using Reactant
 using Adapt
 
 include("Utils.jl")
 
-@testset "Unit tests" verbose = true begin
-    include("Helpers_test.jl")
-    include("Tensor_test.jl")
-    include("Numerics_test.jl")
-    include("TensorNetwork_test.jl")
-    include("Transformations_test.jl")
-    include("Lane_test.jl")
-    include("Site_test.jl")
-    include("Moment_test.jl")
-    include("Quantum_test.jl")
-    include("Gate_test.jl")
-    include("Circuit_test.jl")
-    include("Lattice_test.jl")
-    include("Ansatz_test.jl")
-    include("Product_test.jl")
-    include("MPS_test.jl")
-    include("MPO_test.jl")
+const TENET_TEST_GROUP = lowercase(get(ENV, "TENET_TEST_GROUP", "all"))
+
+if TENET_TEST_GROUP == "all" || TENET_TEST_GROUP == "unit"
+    @testset "Unit tests" verbose = true begin
+        include("Helpers_test.jl")
+        include("Tensor_test.jl")
+        include("Numerics_test.jl")
+        include("TensorNetwork_test.jl")
+        include("Transformations_test.jl")
+        include("Lane_test.jl")
+        include("Site_test.jl")
+        include("Moment_test.jl")
+        include("Quantum_test.jl")
+        include("Gate_test.jl")
+        include("Circuit_test.jl")
+        include("Lattice_test.jl")
+        include("Ansatz_test.jl")
+        include("Product_test.jl")
+        include("MPS_test.jl")
+        include("MPO_test.jl")
+    end
 end
 
-@testset "Integration tests" verbose = true begin
-    include("integration/Reactant_test.jl")
-    include("integration/ChainRules_test.jl")
-    # include("integration/BlockArray_test.jl")
-    include("integration/Dagger_test.jl")
-    include("integration/Makie_test.jl")
-    include("integration/KrylovKit_test.jl")
-    include("integration/Quac_test.jl")
-    include("integration/ITensors_test.jl")
-    include("integration/ITensorNetworks_test.jl")
+if TENET_TEST_GROUP == "all" || TENET_TEST_GROUP == "integration"
+    @testset "Integration tests" verbose = true begin
+        include("integration/Reactant_test.jl")
+        include("integration/ChainRules_test.jl")
+        # include("integration/BlockArray_test.jl")
+        include("integration/Dagger_test.jl")
+        include("integration/Makie_test.jl")
+        include("integration/KrylovKit_test.jl")
+        include("integration/Quac_test.jl")
+        include("integration/ITensors_test.jl")
+        include("integration/ITensorNetworks_test.jl")
 
-    @testset "Python" begin
-        include("integration/python/test_quimb.jl")
-        include("integration/python/test_qiskit.jl")
-        include("integration/python/test_qibo.jl")
+        @testset "Python" begin
+            include("integration/python/test_quimb.jl")
+            include("integration/python/test_qiskit.jl")
+            include("integration/python/test_qibo.jl")
+        end
     end
 end
 
