02857d6d1a71d943d44c68a675bf4934a216f1c0
Todorbsc <todor.krasimirov@bsc.es>
2025-01-20 16:54:38 +0100

    Fix Lane conversion for LineQubit and GridQubit types

--- ext/TenetPythonCallExt/Cirq.jl
+++ ext/TenetPythonCallExt/Cirq.jl
@@ -1,8 +1,8 @@
-function Base.convert(::Type{Lane}, ::Val{Symbol("cirq.devices.line_qubit.LineQubit")}, pyobj::Py)
+function Base.convert(::Type{Lane}, ::Val{Symbol("cirq.LineQubit")}, pyobj::Py)
     Lane(pyconvert(Int, pyobj.x))
 end
 
-function Base.convert(::Type{Lane}, ::Val{Symbol("cirq.devices.grid_qubit.GridQubit")}, pyobj::Py)
+function Base.convert(::Type{Lane}, ::Val{Symbol("cirq.GridQubit")}, pyobj::Py)
     Lane((pyconvert(Int, pyobj.row), pyconvert(Int, pyobj.col)))
 end
 
@@ -16,8 +16,18 @@ function Base.convert(::Type{Circuit}, ::Val{:cirq}, pyobj::Py)
 
     for moment in pyobj
         for gate in moment.operations
-            gatelanes = convert.(Lane, gate.qubits)
-            gatesites = [Site.(gatelanes; dual=true)..., Site.(gatelanes)...]
+            gatelanes = [ 
+                if pyisinstance(qubit, cirq.devices.line_qubit.LineQubit)
+                    convert(Lane, Val(Symbol("cirq.LineQubit")), qubit)
+                elseif pyisinstance(qubit, cirq.devices.grid_qubit.GridQubit)
+                    convert(Lane, Val(Symbol("cirq.GridQubit")), qubit)
+                else
+                    error("Unsupported qubit type: $(pytype(qubit))")
+                end for qubit in gate.qubits
+            ]
+                    
+            #gatelanes = convert.(Lane, gate.qubits)
+            gatesites = [Site.(gatelanes)..., Site.(gatelanes; dual=true)...]
 
             matrix = pyconvert(Array, cirq.unitary(gate))
             array = reshape(matrix, fill(2, length(gatesites))...)
