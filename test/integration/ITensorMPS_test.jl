08b9ed2c114324270ce7df99209e051145d67846
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-02-03 22:06:55 +0100

    Fix symbol ref to `ITensorsMPS.tensors`

--- test/integration/ITensorMPS_test.jl
+++ test/integration/ITensorMPS_test.jl
@@ -8,9 +8,9 @@ using ITensors: ITensor, Index, dim, dims
 tenet_mps = rand(MPS; n=5, maxdim=30)
 itensor_mps = convert(ITensorMPS.MPS, tenet_mps)
 
-@test length(Tenet.tensors(tenet_mps)) == length(ITensors.tensors(itensor_mps))
+@test length(Tenet.tensors(tenet_mps)) == length(ITensorMPS.tensors(itensor_mps))
 
-for (t1, t2) in zip(Tenet.tensors(tenet_mps), ITensors.tensors(itensor_mps))
+for (t1, t2) in zip(Tenet.tensors(tenet_mps), ITensorMPS.tensors(itensor_mps))
     @test issetequal(size(t1), dims(t2))
 end
 
@@ -25,9 +25,9 @@ permuted = permutedims(contracted, [Tenet.inds(tenet_mps; at=Site(i)) for i in 1
 itensor_mps = ITensorMPS.random_mps(siteinds(4, 5); linkdims=7)
 tenet_mps = convert(MPS, itensor_mps)
 
-@test length(ITensors.tensors(itensor_mps)) == length(Tenet.tensors(tenet_mps))
+@test length(ITensorMPS.tensors(itensor_mps)) == length(Tenet.tensors(tenet_mps))
 
-for (t1, t2) in zip(ITensors.tensors(itensor_mps), Tenet.tensors(tenet_mps))
+for (t1, t2) in zip(ITensorMPS.tensors(itensor_mps), Tenet.tensors(tenet_mps))
     @test issetequal(dims(t1), size(t2))
 end
 
