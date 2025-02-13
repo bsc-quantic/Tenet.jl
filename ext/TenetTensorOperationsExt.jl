f0eb6a7edb804ec940e8b14fdebd27fd709bcd24
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-02-13 01:19:47 +0100

    Add TensorOperations backend

---
+++ ext/TenetTensorOperationsExt.jl
@@ -0,0 +1,50 @@
+module TenetTensorOperationsExt
+
+using Tenet
+using Tenet: TensorOperationsBackend
+using TensorOperations: TensorOperations
+
+# TODO we need to check edge cases not supported by TensorOperations and either error or dispatch to a different backend
+
+function Tenet.contract(::TensorOperationsBackend, a::Tensor, b::Tensor; dims=(∩(inds(a), inds(b))), out=nothing)
+    ia = collect(inds(a))
+    ib = collect(inds(b))
+    i = ∩(dims, ia, ib)
+    ic::Vector{Symbol} = if isnothing(out)
+        setdiff(ia ∪ ib, i isa Base.AbstractVecOrTuple ? i : (i,))::Vector{Symbol}
+    else
+        out
+    end
+
+    data = tensorcontract(Tuple(ic), parent(a), Tuple(inds(a)), false, parent(b), Tuple(inds(b)), false)
+    return Tensor(data, ic)
+end
+
+function Tenet.contract(::TensorOperationsBackend, a::Tensor; dims=nonunique(inds(a)), out=nothing)
+    ia = inds(a)
+    i = ∩(dims, ia)
+
+    ic::Vector{Symbol} = if isnothing(out)
+        setdiff(ia, i isa Base.AbstractVecOrTuple ? i : (i,))
+    else
+        out
+    end
+
+    # TODO might fail on partial trace
+    data = tensortrace(Tuple(ic), parent(a), Tuple(inds(a)), false)
+    return Tensor(data, ic)
+end
+
+function Tenet.contract!(::TensorOperationsBackend, c::Tensor, a::Tensor, b::Tensor)
+    pA, pB, pAB = contract_indices(Tuple(inds(a)), Tuple(inds(b)), Tuple(inds(c)))
+    tensorcontract!(parent(c), parent(a), pA, false, parent(b), pB, false, pAB)
+    return c
+end
+
+function Tenet.contract!(::TensorOperationsBackend, y::Tensor, x::Tensor)
+    p, q = TensorOperations.trace_indices(Tuple(inds(x)), Tuple(inds(y)))
+    tensortrace!(parent(y), parent(x), p, q, false)
+    return y
+end
+
+end
