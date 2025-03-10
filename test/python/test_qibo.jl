0c2ad3eb12636e221c9a7a1ee7943949c26711b9
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2025-03-10 12:56:43 +0100

    Move Python integration tests to their own folder (#340)
    * Move Python integration tests to their own directory
    
    It eases the management of the tests, and resolves some caching conflicts.
    
    * Define python tests job on CI
    
    * Rename cache names
    
    * Add warning notice
    
    * Remove `--inline` flag
    
    * Test Python on latest stable Julia version
    
    * Add condition for warning notice
    
    * Try fix environment instantiation
    
    * Refactor testset wrapping
    
    * Fix julia versioning
    
    * fix path

---
+++ test/python/test_qibo.jl
@@ -0,0 +1,23 @@
+using Test
+using Tenet
+using PythonCall
+qibo = pyimport("qibo")
+
+circuit = qibo.Circuit(3)
+circuit.add(qibo.gates.H(0))
+circuit.add(qibo.gates.H(1))
+circuit.add(qibo.gates.CNOT(1, 2))
+circuit.add(qibo.gates.CNOT(0, 2))
+circuit.add(qibo.gates.H(0))
+circuit.add(qibo.gates.H(1))
+circuit.add(qibo.gates.H(2))
+
+circ = convert(Circuit, circuit)
+@test circ isa Circuit
+@test issetequal(sites(circ; set=:inputs), Site.([0, 1, 2]; dual=true))
+@test issetequal(sites(circ; set=:outputs), Site.([0, 1, 2]))
+@test Tenet.ntensors(circ) == 7
+@test issetequal(
+    moments(circ),
+    [Tenet.Moment.(Ref(Lane(0)), 1:4)..., Tenet.Moment.(Ref(Lane(1)), 1:4)..., Tenet.Moment.(Ref(Lane(2)), 1:4)...],
+)
