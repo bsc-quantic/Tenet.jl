23b75a60072974aa89fa4e960f941d707121f53a
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-07-10 12:57:45 +0200

    fix test

--- test/unit/mps.jl
+++ test/unit/mps.jl
@@ -52,7 +52,7 @@ end
     @test issetequal(sites(b), [site"1", site"2", site"3"])
     @test issetequal(bonds(b), [bond"1-2", bond"2-3"])
     @test issetequal(plugs(b), [plug"1", plug"2", plug"3"])
-    @test parent(b[site"1"]) == [1.0, 0.0]
-    @test parent(b[site"2"]) == [0.0, 1.0]
-    @test parent(b[site"3"]) == [1.0, 1.0] / sqrt(2)
+    @test vec(parent(b[site"1"])) == [1.0, 0.0]
+    @test vec(parent(b[site"2"])) == [0.0, 1.0]
+    @test vec(parent(b[site"3"])) == [1.0, 1.0] / sqrt(2)
 end
