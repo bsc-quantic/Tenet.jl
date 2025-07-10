3a6cf81fb2f4a44dba55432a93f041349aca5ae3
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2025-07-10 13:07:40 +0200

    Convert from `ProductState` to `MPS` (#363)
    * Implement conversion from `ProductState` to `MPS`
    
    * Test conversion
    
    * Fix reference
    
    * Fix `setbond!` at boundaries
    
    * fix test
    
    * Fix logic for `setbond!`

--- test/unit/mps.jl
+++ test/unit/mps.jl
@@ -44,3 +44,15 @@ end
     @test size(tn, tn[bond"1-2"]) == 1
     @test size(tn, tn[bond"2-3"]) == 3
 end
+
+@testset "conversion: ProductState -> MPS" begin
+    a = ProductState([[1.0, 0.0], [0.0, 1.0], [1.0, 1.0] / sqrt(2)])
+    b = convert(MPS, a)
+
+    @test issetequal(sites(b), [site"1", site"2", site"3"])
+    @test issetequal(bonds(b), [bond"1-2", bond"2-3"])
+    @test issetequal(plugs(b), [plug"1", plug"2", plug"3"])
+    @test vec(parent(b[site"1"])) == [1.0, 0.0]
+    @test vec(parent(b[site"2"])) == [0.0, 1.0]
+    @test vec(parent(b[site"3"])) == [1.0, 1.0] / sqrt(2)
+end
