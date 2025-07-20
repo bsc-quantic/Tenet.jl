bba73280854d7327689a14ebf1c6155f3eccca7c
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-07-20 13:00:37 +0200

    Refactor `compress!`

--- src/Operations/compress.jl
+++ src/Operations/compress.jl
@@ -2,51 +2,54 @@ using Muscle
 
 function compress! end
 
-function compress!(ψ::MPS; maxdim=nothing, threshold=nothing, kwargs...)
+function generic_mps_compress!(tn; maxdim=nothing, threshold=nothing, kwargs...)
+    n = count(s -> s isa CartesianSite, Tangles.all_sites_iter(tn))
+    for i in 1:(n - 1)
+        generic_mps_compress!(tn, bond"$i-$(i+1)"; kwargs...)
+    end
+    return tn
+end
+
+function generic_mps_compress!(tn, _bond; maxdim=nothing, threshold=nothing, kwargs...)
     @argcheck !isnothing(maxdim) || !isnothing(threshold) "Either `maxdim` or `threshold` must be specified."
     @argcheck isnothing(maxdim) || maxdim > 0 "maxdim must be a positive integer."
     @argcheck isnothing(threshold) || threshold > 0 "Threshold must be positive."
 
-    canonize!(ψ, MixedCanonical(site"1"))
+    bondind = ind_at(tn, _bond)
+    canonize!(tn, BondCanonical(_bond))
 
-    for i in 1:(nsites(ψ) - 1)
-        _bond = bond"$i-$(i+1)"
-        a = tensor_at(ψ, site"$i")
-        b = tensor_at(ψ, site"$(i + 1)")
+    old_u = tensor_at(tn, site"$i")
+    old_v = tensor_at(tn, site"$(i + 1)")
+    old_s = tensor_at(tn, LambdaSite(_bond))
 
-        ind_tmp = Index(gensym(:tmp))
-        U, S, V = Muscle.tensor_svd_thin(a; inds_v=[ind_at(ψ, _bond)], ind_s=ind_tmp)
-        if !isnothing(maxdim)
-            U = view(U, ind_tmp => 1:min(maxdim, length(S)))
-            S = view(S, ind_tmp => 1:min(maxdim, length(S)))
-            V = view(V, ind_tmp => 1:min(maxdim, length(S)))
-        end
+    new_u = old_u
+    new_v = old_v
+    new_s = old_s
 
-        # use `threshold` to truncate the singular values
-        if !isnothing(threshold)
-            keep = findall(x -> abs(x) > threshold, S)
-            U = view(U, ind_tmp => keep)
-            S = view(S, ind_tmp => keep)
-            V = view(V, ind_tmp => keep)
-        end
-
-        # absorb the singular values into V to shift right the orthogonality center
-        V = hadamard!(V, V, S)
-
-        # contract against tensor in next site
-        V = binary_einsum(V, b; dims=[ind_at(ψ, _bond)])
-
-        # rename the temporal index to the bond index
-        V = replace(V, ind_tmp => ind_at(ψ, _bond))
-        U = replace(U, ind_tmp => ind_at(ψ, _bond))
-
-        # update the tensor network
-        @unsafe_region ψ begin
-            replace_tensor!(ψ, a, U)
-            replace_tensor!(ψ, b, V)
-        end
-        unsafe_setform!(ψ, MixedCanonical(site"$(i + 1)"))
+    # use `maxdim` to truncate the singular values
+    if !isnothing(maxdim)
+        new_u = view(new_u, bonind => 1:min(maxdim, length(old_s)))
+        new_v = view(new_v, bonind => 1:min(maxdim, length(old_s)))
+        new_s = view(new_s, bonind => 1:min(maxdim, length(old_s)))
     end
 
-    return ψ
+    # use `threshold` to truncate the singular values
+    if !isnothing(threshold)
+        keep = findall(x -> abs(x) > threshold, S)
+        new_u = view(new_u, bondind => keep)
+        new_v = view(new_v, bondind => keep)
+        new_s = view(new_s, bondind => keep)
+    end
+
+    # update the tensor network
+    @unsafe_region tn begin
+        replace_tensor!(tn, old_u, new_u)
+        replace_tensor!(tn, old_v, new_v)
+        replace_tensor!(tn, old_s, new_s)
+    end
+
+    return tn
 end
+
+compress!(tn::MPS; kwargs...) = generic_mps_compress!(tn, kwargs...)
+compress!(tn::MPO; kwargs...) = generic_mps_compress!(tn, kwargs...)
