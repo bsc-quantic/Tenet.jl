cf2ecf6c7ff55eb7e2396a6fca65c118ba7c3a57
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-07-23 12:58:27 +0200

    Correctly parallelize test groups

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,19 @@ using Test
 using SafeTestsets
 using Tenet
 
-@testset "Unit" verbose = true begin
-    @safetestset "ProductState" include("unit/product_state.jl")
-    @safetestset "ProductOperator" include("unit/product_operator.jl")
-    @safetestset "MatrixProductState" include("unit/mps.jl")
-    @safetestset "MatrixProductOperator" include("unit/mpo.jl")
+TEST_GROUP = get(ENV, "TENET_TEST_GROUP", "all")
+
+if TEST_GROUP == "all" || TEST_GROUP == "unit"
+    @testset "Unit" verbose = true begin
+        @safetestset "ProductState" include("unit/product_state.jl")
+        @safetestset "ProductOperator" include("unit/product_operator.jl")
+        @safetestset "MatrixProductState" include("unit/mps.jl")
+        @safetestset "MatrixProductOperator" include("unit/mpo.jl")
+    end
 end
 
-@testset "Integration" verbose = true begin
-    @safetestset "ITensorMPS" include("integration/itensormps.jl")
+if TEST_GROUP == "all" || TEST_GROUP == "integration"
+    @testset "Integration" verbose = true begin
+        @safetestset "ITensorMPS" include("integration/itensormps.jl")
+    end
 end
