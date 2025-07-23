90ebd048322341d1d60fe9d02decf7654ded2927
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-07-23 12:48:10 +0200

    Trigger tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,3 +8,7 @@ using Tenet
     @safetestset "MatrixProductState" include("unit/mps.jl")
     @safetestset "MatrixProductOperator" include("unit/mpo.jl")
 end
+
+@testset "Integration" verbose = true begin
+    @safetestset "ITensorMPS" include("integration/itensormps.jl")
+end
